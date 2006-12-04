f69cc749a89b9abb67a4747396db4037b473a05e
fcorneli <fcorneli@cd7dd898-ab41-4057-98bc-1513970a52e4>
2006-12-04 15:22:29 +0000

    added Tomcat login valve: now the seam pages (view in MVC) can also access the authenticated user via userPrincipal
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@76 cd7dd898-ab41-4057-98bc-1513970a52e4

---
+++ safe-online-j2ee-util/src/main/java/net/link/safeonline/util/valve/LoginValve.java
@@ -0,0 +1,46 @@
+package net.link.safeonline.util.valve;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.catalina.Valve;
+import org.apache.catalina.connector.Request;
+import org.apache.catalina.connector.Response;
+import org.apache.catalina.valves.ValveBase;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jboss.security.SimplePrincipal;
+
+public class LoginValve extends ValveBase {
+
+	private static final Log LOG = LogFactory.getLog(LoginValve.class);
+
+	public LoginValve() {
+		LOG.debug("login valve construction");
+	}
+
+	@Override
+	public void invoke(Request request, Response response) throws IOException,
+			ServletException {
+		LOG.debug("login valve invoked");
+
+		HttpServletRequest httpServletRequest = request.getRequest();
+
+		HttpSession httpSession = httpServletRequest.getSession(false);
+		if (null != httpSession) {
+			LOG.debug("http session present");
+			String username = (String) httpSession.getAttribute("username");
+			if (null != username) {
+				LOG.debug("setting user principal to " + username);
+				request.setUserPrincipal(new SimplePrincipal(username));
+			}
+		}
+		Valve valve = this.getNext();
+		if (null != valve) {
+			valve.invoke(request, response);
+		}
+	}
+}
