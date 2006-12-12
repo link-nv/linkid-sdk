8f6dc42b9487aebf8a72b4eabf9d7a4daa4127c0
fcorneli <fcorneli@cd7dd898-ab41-4057-98bc-1513970a52e4>
2006-12-12 15:08:10 +0000

    add: safe-online-sql-ddl project to generate SQL DDL file out of annotated EJB3 entities
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@97 cd7dd898-ab41-4057-98bc-1513970a52e4

--- safe-online-entity/src/main/java/net/link/safeonline/entity/SubjectEntity.java
+++ safe-online-entity/src/main/java/net/link/safeonline/entity/SubjectEntity.java
@@ -18,7 +18,7 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 
 @Entity
-@Table(name = "entity")
+@Table(name = "subject")
 public class SubjectEntity implements Serializable {
 
 	private static final long serialVersionUID = 1L;
