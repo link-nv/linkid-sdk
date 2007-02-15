bfa19d6ba9915e8201dc5f0d0dcf01360238dcbc
fcorneli <fcorneli@cd7dd898-ab41-4057-98bc-1513970a52e4>
2007-02-15 15:00:54 +0000

    updated JBoss Seam to 1.1.6.GA updated JAX-WS RI to 2.1
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@156 cd7dd898-ab41-4057-98bc-1513970a52e4

--- safe-online-auth-ws-impl/src/test/java/test/unit/net/link/safeonline/auth/ws/SafeOnlineAuthenticationPortImplTest.java
+++ safe-online-auth-ws-impl/src/test/java/test/unit/net/link/safeonline/auth/ws/SafeOnlineAuthenticationPortImplTest.java
@@ -114,7 +114,7 @@ public class SafeOnlineAuthenticationPortImplTest extends TestCase {
 		assertTrue(result.isAuthenticated());
 	}
 
-	public void __testEndpointViaJAXWSRI() throws Exception {
+	public void testEndpointViaJAXWSRI() throws Exception {
 		// setup
 		SafeOnlineAuthenticationPort wsPort = new SafeOnlineAuthenticationPortImpl();
 		Endpoint endpoint = Endpoint.create(wsPort);
