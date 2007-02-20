bdb501415aa43bf06f24e2f9c9dc885a789158a8
fcorneli <fcorneli@cd7dd898-ab41-4057-98bc-1513970a52e4>
2007-02-20 14:26:32 +0000

    add: attribute web service definition project
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@159 cd7dd898-ab41-4057-98bc-1513970a52e4

---
+++ safe-online-attrib-ws/src/test/java/test/unit/net/link/safeonline/attrib/ws/SAMLAttributeServiceFactoryTest.java
@@ -0,0 +1,23 @@
+/*
+ * SafeOnline project.
+ * 
+ * Copyright 2006-2007 Lin.k N.V. All rights reserved.
+ * Lin.k N.V. proprietary/confidential. Use is subject to license terms.
+ */
+
+package test.unit.net.link.safeonline.attrib.ws;
+
+import net.link.safeonline.attrib.ws.SAMLAttributeServiceFactory;
+import oasis.names.tc.saml._2_0.protocol.SAMLAttributeService;
+import junit.framework.TestCase;
+
+public class SAMLAttributeServiceFactoryTest extends TestCase {
+
+	public void testNewInstance() throws Exception {
+		// operate
+		SAMLAttributeService result = SAMLAttributeServiceFactory.newInstance();
+
+		// verify
+		assertNotNull(result);
+	}
+}
