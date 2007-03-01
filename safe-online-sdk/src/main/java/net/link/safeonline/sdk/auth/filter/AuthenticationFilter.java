224029e1b572078f17f2d70e546772983d2516eb
fcorneli <fcorneli@cd7dd898-ab41-4057-98bc-1513970a52e4>
2007-03-01 10:42:20 +0000

    refactor: moved authentication filter from demo webapp to sdk change: authentication filter is using http 302 redirect now to redirect to the auth web app add: authentication filter javadoc
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@191 cd7dd898-ab41-4057-98bc-1513970a52e4

--- safe-online-sdk/src/main/java/net/link/safeonline/sdk/auth/filter/AuthenticationFilter.java
+++ safe-online-sdk/src/main/java/net/link/safeonline/sdk/auth/filter/AuthenticationFilter.java
@@ -1,14 +1,13 @@
 /*
  * SafeOnline project.
  * 
- * Copyright 2006 Lin.k N.V. All rights reserved.
+ * Copyright 2006-2007 Lin.k N.V. All rights reserved.
  * Lin.k N.V. proprietary/confidential. Use is subject to license terms.
  */
 
-package net.link.safeonline.demo.filter;
+package net.link.safeonline.sdk.auth.filter;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.URLEncoder;
 
 import javax.servlet.Filter;
@@ -17,16 +16,31 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.UnavailableException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+/**
+ * SafeOnline Authentication Filter. This filter can be used by servlet
+ * container based web application for authentication via SafeOnline. The
+ * configuration of this filter should be managed via the web.xml deployment
+ * descriptor.
+ * 
+ * @author fcorneli
+ * 
+ */
 public class AuthenticationFilter implements Filter {
 
 	private static final Log LOG = LogFactory
 			.getLog(AuthenticationFilter.class);
+
+	public static final String AUTH_SERVICE_URL_INIT_PARAM = "SafeOnlineAuthenticationServiceUrl";
+
+	public static final String APPLICATION_NAME_INIT_PARAM = "ApplicationName";
 
 	private String safeOnlineAuthenticationServiceUrl;
 
@@ -35,12 +49,21 @@ public class AuthenticationFilter implements Filter {
 	public void init(FilterConfig config) throws ServletException {
 		LOG.debug("init");
 		this.safeOnlineAuthenticationServiceUrl = config
-				.getInitParameter("SafeOnlineAuthenticationServiceUrl");
-		this.applicationName = config.getInitParameter("ApplicationName");
+				.getInitParameter(AUTH_SERVICE_URL_INIT_PARAM);
+		this.applicationName = config
+				.getInitParameter(APPLICATION_NAME_INIT_PARAM);
 		LOG
 				.debug("redirection url: "
 						+ this.safeOnlineAuthenticationServiceUrl);
 		LOG.debug("application name: " + this.applicationName);
+		if (null == this.safeOnlineAuthenticationServiceUrl) {
+			throw new UnavailableException(AUTH_SERVICE_URL_INIT_PARAM
+					+ " init param should be specified in web.xml");
+		}
+		if (null == this.applicationName) {
+			throw new UnavailableException(APPLICATION_NAME_INIT_PARAM
+					+ " init param should be specified in web.xml");
+		}
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response,
@@ -48,11 +71,13 @@ public class AuthenticationFilter implements Filter {
 		LOG.debug("doFilter");
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		HttpSession session = httpRequest.getSession();
+
 		String paramUsername = httpRequest.getParameter("username");
 		if (null != paramUsername) {
 			LOG.debug("doing a login via the SafeOnline username token");
 			session.setAttribute("username", paramUsername);
 		}
+
 		String username = (String) session.getAttribute("username");
 		if (null == username) {
 			outputRedirectPage(httpRequest, response);
@@ -63,33 +88,15 @@ public class AuthenticationFilter implements Filter {
 
 	private void outputRedirectPage(HttpServletRequest request,
 			ServletResponse response) throws IOException {
-		PrintWriter out = response.getWriter();
+		LOG.debug("redirecting to: " + this.safeOnlineAuthenticationServiceUrl);
+		HttpServletResponse httpServletResponse = (HttpServletResponse) response;
 		String redirectUrl = this.safeOnlineAuthenticationServiceUrl
 				+ "?application="
 				+ URLEncoder.encode(this.applicationName, "UTF-8")
 				+ "&target="
 				+ URLEncoder
 						.encode(request.getRequestURL().toString(), "UTF-8");
-		LOG.debug("redirect url: " + redirectUrl);
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>SafeOnline Demo</title>");
-		out.println("<script language=\"JavaScript\">");
-		out.println("<!--");
-		out.println("redirectionTime = \"1000\";");
-		out.println("redirectionUrl = \"" + redirectUrl + "\";");
-		out.println("function redirectionTimer() {");
-		out
-				.println("self.setTimeout(\"self.location.href = redirectionUrl;\",redirectionTime);");
-		out.println("}");
-		out.println("// -->");
-		out.println("</script>");
-		out.println("</head>");
-		out.println("<body onLoad=\"redirectionTimer()\">");
-		out
-				.println("<h1>Redirecting to the SafeOnline Authentication Service...</h1>");
-		out.println("</body>");
-		out.println("</html>");
+		httpServletResponse.sendRedirect(redirectUrl);
 	}
 
 	public void destroy() {
