9521e05a883683362e9948e4760099d0cb4141ba
fcorneli <fcorneli@cd7dd898-ab41-4057-98bc-1513970a52e4>
2007-03-08 17:48:44 +0000

    fix: each task must have its own transaction
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@213 cd7dd898-ab41-4057-98bc-1513970a52e4

--- safe-online-model-demo/src/main/java/net/link/safeonline/model/demo/DemoTaskBean.java
+++ safe-online-model-demo/src/main/java/net/link/safeonline/model/demo/DemoTaskBean.java
@@ -11,6 +11,8 @@ import java.util.Date;
 
 import javax.ejb.Local;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -29,6 +31,7 @@ public class DemoTaskBean implements Task {
 		return "Human readable demo task";
 	}
 
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
 	public void perform() {
 		Date now = new Date();
 		LOG.debug("Demo task perform: " + now);
