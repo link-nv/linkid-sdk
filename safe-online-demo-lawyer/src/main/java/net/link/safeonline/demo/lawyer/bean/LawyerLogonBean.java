b2f78bdcb5c6edaed9b62e084cd8d56d7cfb7edf
fcorneli <fcorneli@cd7dd898-ab41-4057-98bc-1513970a52e4>
2007-04-11 11:50:14 +0000

    add: demo lawyer webapp artifact
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@298 cd7dd898-ab41-4057-98bc-1513970a52e4

---
+++ safe-online-demo-lawyer/src/main/java/net/link/safeonline/demo/lawyer/bean/LawyerLogonBean.java
@@ -0,0 +1,59 @@
+/*
+ * SafeOnline project.
+ * 
+ * Copyright 2006-2007 Lin.k N.V. All rights reserved.
+ * Lin.k N.V. proprietary/confidential. Use is subject to license terms.
+ */
+
+package net.link.safeonline.demo.lawyer.bean;
+
+import javax.ejb.Remove;
+import javax.ejb.Stateful;
+
+import net.link.safeonline.demo.lawyer.LawyerLogon;
+import net.link.safeonline.sdk.auth.seam.SafeOnlineLoginUtils;
+
+import org.jboss.annotation.ejb.LocalBinding;
+import org.jboss.annotation.ejb.cache.simple.CacheConfig;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.Seam;
+import org.jboss.seam.annotations.Destroy;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.core.FacesMessages;
+import org.jboss.seam.log.Log;
+
+@Stateful
+@Name("lawyerLogon")
+@Scope(ScopeType.SESSION)
+@CacheConfig(idleTimeoutSeconds = (5 + 1) * 60)
+@LocalBinding(jndiBinding = "SafeOnlineLawyerDemo/LawyerLogonBean/local")
+public class LawyerLogonBean implements LawyerLogon {
+
+	@Logger
+	private Log log;
+
+	@In(create = true)
+	private FacesMessages facesMessages;
+
+	@Remove
+	@Destroy
+	public void destroyCallback() {
+		// empty
+	}
+
+	public String login() {
+		log.debug("login");
+		String result = SafeOnlineLoginUtils.login(this.facesMessages,
+				this.log, "login");
+		return result;
+	}
+
+	public String logout() {
+		log.debug("logout");
+		Seam.invalidateSession();
+		return "logout-success";
+	}
+}
