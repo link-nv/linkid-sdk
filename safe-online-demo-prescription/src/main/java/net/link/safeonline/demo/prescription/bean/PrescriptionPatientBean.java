ecfabe03631a8be2df22dd1af29f3c12d35105d1
fcorneli <fcorneli@cd7dd898-ab41-4057-98bc-1513970a52e4>
2007-05-05 09:18:56 +0000

    add: patient can get an overview of his prescriptions
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@364 cd7dd898-ab41-4057-98bc-1513970a52e4

--- safe-online-demo-prescription/src/main/java/net/link/safeonline/demo/prescription/bean/PrescriptionPatientBean.java
+++ safe-online-demo-prescription/src/main/java/net/link/safeonline/demo/prescription/bean/PrescriptionPatientBean.java
@@ -8,7 +8,6 @@
 package net.link.safeonline.demo.prescription.bean;
 
 import java.security.Principal;
-import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -20,12 +19,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import net.link.safeonline.demo.prescription.PrescriptionConstants;
+import net.link.safeonline.demo.prescription.PrescriptionPatient;
+import net.link.safeonline.demo.prescription.entity.PrescriptionEntity;
+
 import org.jboss.annotation.ejb.LocalBinding;
 import org.jboss.annotation.security.SecurityDomain;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Destroy;
 import org.jboss.seam.annotations.Factory;
-import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Out;
@@ -33,38 +35,26 @@ import org.jboss.seam.annotations.datamodel.DataModel;
 import org.jboss.seam.annotations.datamodel.DataModelSelection;
 import org.jboss.seam.log.Log;
 
-import net.link.safeonline.demo.prescription.PrescriptionConstants;
-import net.link.safeonline.demo.prescription.PrescriptionFill;
-import net.link.safeonline.demo.prescription.entity.PrescriptionEntity;
-
 @Stateful
-@Name("prescriptionFill")
-@LocalBinding(jndiBinding = "SafeOnlinePrescriptionDemo/PrescriptionFillBean/local")
+@Name("prescriptionPatient")
+@LocalBinding(jndiBinding = "SafeOnlinePrescriptionDemo/PrescriptionPatientBean/local")
 @SecurityDomain(PrescriptionConstants.SECURITY_DOMAIN)
-public class PrescriptionFillBean implements PrescriptionFill {
+public class PrescriptionPatientBean implements PrescriptionPatient {
 
 	@Logger
 	private Log log;
 
-	public final static String PRESCRIPTIONS = "prescriptions";
+	public final static String PATIENT_PRESCRIPTIONS = "patientPrescriptions";
 
 	public final static String SELECTED_PRESCRIPTION = "selectedPrescription";
 
 	@SuppressWarnings("unused")
-	@DataModel(PRESCRIPTIONS)
+	@DataModel(PATIENT_PRESCRIPTIONS)
 	private List<PrescriptionEntity> prescriptions;
 
-	@DataModelSelection(PRESCRIPTIONS)
+	@DataModelSelection(PATIENT_PRESCRIPTIONS)
 	@Out(value = SELECTED_PRESCRIPTION, required = false, scope = ScopeType.SESSION)
 	private PrescriptionEntity selectedPrescription;
-
-	/**
-	 * Fill prescription entity. We have to use a field different from
-	 * selectedPrescription because the DataModelSelection annotation is causing
-	 * the selectedPrescription field to become null.
-	 */
-	@In(value = SELECTED_PRESCRIPTION, required = false)
-	private PrescriptionEntity fillPrescription;
 
 	@Remove
 	@Destroy
@@ -74,40 +64,25 @@ public class PrescriptionFillBean implements PrescriptionFill {
 	@PersistenceContext(unitName = PrescriptionConstants.ENTITY_MANAGER)
 	private EntityManager entityManager;
 
-	@In(value = "patient")
-	private String patient;
-
-	@SuppressWarnings("unchecked")
-	@RolesAllowed(PrescriptionConstants.PHARMACIST_ROLE)
-	@Factory(PRESCRIPTIONS)
-	public void prescriptionsFactory() {
-		Query query = this.entityManager
-				.createQuery("SELECT prescription FROM PrescriptionEntity AS prescription "
-						+ "WHERE prescription.patient = :patient AND "
-						+ "prescription.filled = FALSE");
-		query.setParameter("patient", this.patient);
-		this.prescriptions = query.getResultList();
-	}
-
-	@RolesAllowed(PrescriptionConstants.PHARMACIST_ROLE)
-	public String view() {
-		log.debug("view: #0", this.selectedPrescription.getId());
-		return "view";
-	}
-
 	@Resource
 	private SessionContext sessionContext;
 
-	@RolesAllowed(PrescriptionConstants.PHARMACIST_ROLE)
-	public String fill() {
-		log.debug("filling: #0", this.fillPrescription.getId());
-		this.fillPrescription.setFilled(true);
-		Principal pharmacistPrincipal = this.sessionContext
-				.getCallerPrincipal();
-		String pharmacist = pharmacistPrincipal.getName();
-		this.fillPrescription.setPharmacist(pharmacist);
-		this.fillPrescription.setFilledDate(new Date());
-		this.entityManager.merge(this.fillPrescription);
-		return "filled";
+	@SuppressWarnings("unchecked")
+	@RolesAllowed(PrescriptionConstants.PATIENT_ROLE)
+	@Factory(PATIENT_PRESCRIPTIONS)
+	public void patientPrescriptionsFactory() {
+		Principal patientPrincipal = this.sessionContext.getCallerPrincipal();
+		String patient = patientPrincipal.getName();
+		Query query = this.entityManager
+				.createQuery("SELECT prescription FROM PrescriptionEntity AS prescription "
+						+ "WHERE prescription.patient = :patient");
+		query.setParameter("patient", patient);
+		this.prescriptions = query.getResultList();
+	}
+
+	@RolesAllowed(PrescriptionConstants.PATIENT_ROLE)
+	public String view() {
+		log.debug("view: #0", this.selectedPrescription.getId());
+		return "view";
 	}
 }
