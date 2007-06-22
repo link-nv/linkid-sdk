63b894a2da6dc9ab01676b3c7976e769fd127508
fcorneli <fcorneli@cd7dd898-ab41-4057-98bc-1513970a52e4>
2007-06-22 13:53:10 +0000

    add: @Compound annotations (hey, subversion works again)
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@478 cd7dd898-ab41-4057-98bc-1513970a52e4

---
+++ safe-online-sdk/src/main/java/net/link/safeonline/sdk/ws/attrib/annotation/Compound.java
@@ -0,0 +1,33 @@
+/*
+ * SafeOnline project.
+ * 
+ * Copyright 2006-2007 Lin.k N.V. All rights reserved.
+ * Lin.k N.V. proprietary/confidential. Use is subject to license terms.
+ */
+
+package net.link.safeonline.sdk.ws.attrib.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Specifies that the class is a compounded attribute.
+ * 
+ * @author fcorneli
+ * 
+ */
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Compound {
+
+	/**
+	 * The name of the compounded attribute.
+	 * 
+	 * @return
+	 */
+	String value();
+}
---
+++ safe-online-sdk/src/test/java/test/unit/net/link/safeonline/sdk/ws/attrib/CompoundedAttributeTest.java
@@ -0,0 +1,36 @@
+/*
+ * SafeOnline project.
+ * 
+ * Copyright 2006-2007 Lin.k N.V. All rights reserved.
+ * Lin.k N.V. proprietary/confidential. Use is subject to license terms.
+ */
+
+package test.unit.net.link.safeonline.sdk.ws.attrib;
+
+import org.junit.Test;
+
+import net.link.safeonline.sdk.ws.attrib.annotation.Compound;
+import net.link.safeonline.sdk.ws.attrib.annotation.CompoundMember;
+
+public class CompoundedAttributeTest {
+
+	@Compound("test-compound-attribute-name")
+	public static class MyTestCompoundedAttribute {
+
+		private String name;
+
+		@CompoundMember("test-member-attribute-name")
+		public String getName() {
+			return this.name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
+
+	@Test
+	public void instance() throws Exception {
+		new MyTestCompoundedAttribute();
+	}
+}
