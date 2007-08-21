205fe96dfd0b580b54c53999f2cbca8ee5921e4a
fcorneli <fcorneli@cd7dd898-ab41-4057-98bc-1513970a52e4>
2007-08-21 14:42:50 +0000

    add: IdentityService JMX bean to manage the SafeOnline identity key pair. will be used to sign the issued SAML tokens
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@550 cd7dd898-ab41-4057-98bc-1513970a52e4

---
+++ safe-online-service/src/main/java/net/link/safeonline/service/IdentityService.java
@@ -0,0 +1,104 @@
+/*
+ * SafeOnline project.
+ * 
+ * Copyright 2006-2007 Lin.k N.V. All rights reserved.
+ * Lin.k N.V. proprietary/confidential. Use is subject to license terms.
+ */
+
+package net.link.safeonline.service;
+
+import java.io.InputStream;
+import java.security.PrivateKey;
+import java.security.KeyStore.PrivateKeyEntry;
+
+import net.link.safeonline.sdk.KeyStoreUtils;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * JMX bean that manages the identity of the SafeOnline instance. This identity
+ * is used for signing of SAML tokens.
+ * 
+ * <p>
+ * This identity service can be extended later on when other types of identity
+ * tokens must be supported. For example: PKCS#12 resource and file key stores
+ * or HSMs via PKCS#11 drivers.
+ * </p>
+ * 
+ * @author fcorneli
+ * 
+ */
+public class IdentityService implements IdentityServiceMBean {
+
+	private static final Log LOG = LogFactory.getLog(IdentityService.class);
+
+	public IdentityService() {
+		LOG.debug("construction");
+	}
+
+	private String keyStoreResource;
+
+	private String keyStorePassword;
+
+	private String keyStoreType;
+
+	public PrivateKey loadPrivateKey() {
+		LOG.debug("load private key");
+		if (null == this.keyStoreResource) {
+			throw new RuntimeException("no key store resource set");
+		}
+		if (null == this.keyStorePassword) {
+			throw new RuntimeException("no key store password set");
+		}
+		if (null == this.keyStoreType) {
+			throw new RuntimeException("no key store type set");
+		}
+
+		Thread currenThread = Thread.currentThread();
+		ClassLoader classLoader = currenThread.getContextClassLoader();
+		InputStream keyStoreInputStream = classLoader
+				.getResourceAsStream(this.keyStoreResource);
+		if (null == keyStoreInputStream) {
+			throw new RuntimeException("keystore resource not found: "
+					+ this.keyStoreResource);
+		}
+		PrivateKeyEntry privateKeyEntry;
+		try {
+			privateKeyEntry = KeyStoreUtils.loadPrivateKeyEntry(
+					this.keyStoreType, keyStoreInputStream,
+					this.keyStorePassword, this.keyStorePassword);
+		} finally {
+			IOUtils.closeQuietly(keyStoreInputStream);
+		}
+		PrivateKey privateKey = privateKeyEntry.getPrivateKey();
+		return privateKey;
+	}
+
+	public void setKeyStorePassword(String keyStorePassword) {
+		this.keyStorePassword = keyStorePassword;
+	}
+
+	public void setKeyStoreResource(String keyStoreResource) {
+		LOG.debug("set key store resource: " + keyStoreResource);
+		this.keyStoreResource = keyStoreResource;
+	}
+
+	public String getKeyStorePassword() {
+		return this.keyStorePassword;
+	}
+
+	public String getKeyStoreResource() {
+		LOG.debug("get key store resource: " + this.keyStoreResource);
+		return this.keyStoreResource;
+	}
+
+	public String getKeyStoreType() {
+		return this.keyStoreType;
+	}
+
+	public void setKeyStoreType(String keyStoreType) {
+		this.keyStoreType = keyStoreType;
+	}
+}
---
+++ safe-online-service/src/main/java/net/link/safeonline/service/IdentityServiceMBean.java
@@ -0,0 +1,33 @@
+/*
+ * SafeOnline project.
+ * 
+ * Copyright 2006-2007 Lin.k N.V. All rights reserved.
+ * Lin.k N.V. proprietary/confidential. Use is subject to license terms.
+ */
+
+package net.link.safeonline.service;
+
+import java.security.PrivateKey;
+
+/**
+ * Interface for Identity Service JMX bean.
+ * 
+ * @author fcorneli
+ * 
+ */
+public interface IdentityServiceMBean {
+
+	PrivateKey loadPrivateKey();
+
+	void setKeyStoreResource(String keyStoreResource);
+
+	String getKeyStoreResource();
+
+	void setKeyStorePassword(String keyStorePassword);
+
+	String getKeyStorePassword();
+
+	void setKeyStoreType(String keyStoreType);
+
+	String getKeyStoreType();
+}
