82c698ccbfc2eecaff1dbb3e44ebf7d129d934dc
fcorneli <fcorneli@cd7dd898-ab41-4057-98bc-1513970a52e4>
2007-09-03 09:39:58 +0000

    add: timeout filter is using a cookie now to detect 'application session' timeout instead of 'http session' timeout.
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@577 cd7dd898-ab41-4057-98bc-1513970a52e4

---
+++ safe-online-webapp-base/src/main/java/net/link/safeonline/webapp/filter/LogHttpSessionListener.java
@@ -0,0 +1,28 @@
+/*
+ * SafeOnline project.
+ * 
+ * Copyright 2006-2007 Lin.k N.V. All rights reserved.
+ * Lin.k N.V. proprietary/confidential. Use is subject to license terms.
+ */
+
+package net.link.safeonline.webapp.filter;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class LogHttpSessionListener implements HttpSessionListener {
+
+	private static final Log LOG = LogFactory
+			.getLog(LogHttpSessionListener.class);
+
+	public void sessionCreated(HttpSessionEvent event) {
+		LOG.debug("session created: " + event.getSession().getId());
+	}
+
+	public void sessionDestroyed(HttpSessionEvent event) {
+		LOG.debug("session destroyed: " + event.getSession().getId());
+	}
+}
--- safe-online-webapp-base/src/main/java/net/link/safeonline/webapp/filter/TimeoutFilter.java
+++ safe-online-webapp-base/src/main/java/net/link/safeonline/webapp/filter/TimeoutFilter.java
@@ -17,7 +17,10 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.UnavailableException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -25,14 +28,29 @@ import org.apache.commons.logging.LogFactory;
 /**
  * Servlet Filter that handles browser timeout events.
  * 
+ * <p>
+ * The init parameters for this filter are:
+ * </p>
+ * <ul>
+ * <li><code>TimeoutPath</code>: the path to the timeout page.</li>
+ * <li><code>LoginSessionAttribute</code>: the HTTP session attribute that
+ * indicated a logged in user.</li>
+ * </ul>
+ * 
  * @author fcorneli
  * 
  */
 public class TimeoutFilter implements Filter {
 
+	public static final String TIMEOUT_PATH_INIT_PARAM = "TimeoutPath";
+
+	public static final String LOGIN_SESSION_ATTRIBUTE_INIT_PARAM = "LoginSessionAttribute";
+
 	private static final Log LOG = LogFactory.getLog(TimeoutFilter.class);
 
 	private String timeoutPath;
+
+	private String loginSessionAttribute;
 
 	public void destroy() {
 		LOG.debug("destroy");
@@ -56,26 +74,90 @@ public class TimeoutFilter implements Filter {
 			 * This means that the servlet container found a matching session
 			 * context for the requested session Id.
 			 */
+			HttpSession session = httpRequest.getSession();
+			Object loginSessionAttribute = session
+					.getAttribute(this.loginSessionAttribute);
+			if (null != loginSessionAttribute) {
+				if (false == hasLoginCookie(httpRequest)) {
+					HttpServletResponse httpResponse = (HttpServletResponse) response;
+					/*
+					 * We communicate that the user has been properly logged in
+					 * by setting a non-persistent browser cookie.
+					 */
+					Cookie loginCookie = new Cookie("login", "true");
+					httpResponse.addCookie(loginCookie);
+				}
+			} else {
+				/*
+				 * If the user performs a logout, we need to remove the login
+				 * cookie. Else the user could trigger an explicit timeout while
+				 * actually he's no longer logged in.
+				 */
+				removeLoginCookie(httpRequest, response);
+			}
 			chain.doFilter(request, response);
 			return;
 		}
 		/*
 		 * In this case no corresponding session context for the given requested
-		 * session Id was found. This is an indication that the browser caused a
-		 * timeout on the web application.
+		 * session Id was found. This could be an indication that the browser
+		 * caused a timeout on the web application. We detect this via the login
+		 * cookie.
 		 */
-		LOG.debug("forwaring to timeout path: " + this.timeoutPath);
+		if (true == hasLoginCookie(httpRequest)) {
+			LOG.debug("forwaring to timeout path: " + this.timeoutPath);
+			removeLoginCookie(httpRequest, response);
+			RequestDispatcher requestDispatcher = request
+					.getRequestDispatcher(this.timeoutPath);
+			requestDispatcher.forward(request, response);
+			return;
+		}
+		/*
+		 * If no login cookie was found, then the browser indeed caused a
+		 * timeout on the HTTP session, but since the user was not yet logged
+		 * in, it's not that harmful.
+		 */
+		chain.doFilter(request, response);
+	}
 
-		RequestDispatcher requestDispatcher = request
-				.getRequestDispatcher(this.timeoutPath);
-		requestDispatcher.forward(request, response);
+	private void removeLoginCookie(HttpServletRequest httpRequest,
+			ServletResponse response) {
+		if (false == hasLoginCookie(httpRequest)) {
+			return;
+		}
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		Cookie loginCookie = new Cookie("login", "");
+		loginCookie.setMaxAge(0);
+		httpResponse.addCookie(loginCookie);
+	}
+
+	private boolean hasLoginCookie(HttpServletRequest httpRequest) {
+		Cookie[] cookies = httpRequest.getCookies();
+		if (null == cookies) {
+			return false;
+		}
+		for (Cookie cookie : cookies) {
+			if ("login".equals(cookie.getName())) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public void init(FilterConfig config) throws ServletException {
 		LOG.debug("init");
-		this.timeoutPath = config.getInitParameter("TimeoutPath");
-		if (null == this.timeoutPath) {
-			throw new UnavailableException("missing TimeoutPath init parameter");
+		this.timeoutPath = getInitParameter(config, TIMEOUT_PATH_INIT_PARAM);
+		this.loginSessionAttribute = getInitParameter(config,
+				LOGIN_SESSION_ATTRIBUTE_INIT_PARAM);
+	}
+
+	private String getInitParameter(FilterConfig config, String parameterName)
+			throws UnavailableException {
+		String value = config.getInitParameter(parameterName);
+		if (null == value) {
+			throw new UnavailableException("missing init parameter: "
+					+ parameterName);
 		}
+		return value;
 	}
 }
