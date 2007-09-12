0753a6e74cebd09fb0b183e9f0ae0ae589665d9d
fcorneli <fcorneli@cd7dd898-ab41-4057-98bc-1513970a52e4>
2007-09-12 07:42:51 +0000

    add: basic CLI artifact + bundling with the now executable EAR
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@600 cd7dd898-ab41-4057-98bc-1513970a52e4

---
+++ safe-online-cli/src/main/java/net/link/safeonline/cli/Main.java
@@ -0,0 +1,21 @@
+/*
+ * SafeOnline project.
+ * 
+ * Copyright 2006-2007 Lin.k N.V. All rights reserved.
+ * Lin.k N.V. proprietary/confidential. Use is subject to license terms.
+ */
+
+package net.link.safeonline.cli;
+
+/**
+ * SafeOnline Command Line Interface Entry Point.
+ * 
+ * @author fcorneli
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("SafeOnline CLI");
+	}
+}
