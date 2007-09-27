46b31d6c414e9a49619f02cd7c3f5fe93f203168
wvdhaute <wvdhaute@cd7dd898-ab41-4057-98bc-1513970a52e4>
2007-09-27 10:38:21 +0000

    add: web interface exception handling
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@642 cd7dd898-ab41-4057-98bc-1513970a52e4

---
+++ safe-online-webapp-base/src/main/java/net/link/safeonline/webapp/filter/ExceptionFilter.java
@@ -0,0 +1,77 @@
+/*
+ * SafeOnline project.
+ * 
+ * Copyright 2006-2007 Lin.k N.V. All rights reserved.
+ * Lin.k N.V. proprietary/confidential. Use is subject to license terms.
+ */
+
+package net.link.safeonline.webapp.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.UnavailableException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Exception filter that handles all GUI exceptions.
+ * 
+ * <p>
+ * The init parameters for this filter are:
+ * </p>
+ * <ul>
+ * <li><code>ErrorPage</code>: the path to the error page.</li>
+ * </ul>
+ * 
+ * @author wvdhaute
+ * 
+ */
+public class ExceptionFilter implements Filter {
+
+	private static final long serialVersionUID = 1L;
+
+	private static final Log LOG = LogFactory.getLog(ExceptionFilter.class);
+
+	private static final String INIT_PARAM_ERROR_PAGE = "ErrorPage";
+
+	private String errorPage;
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		try {
+			chain.doFilter(request, response);
+		} catch (Throwable e) {
+			RequestDispatcher requestDispatcher = request
+					.getRequestDispatcher(this.errorPage);
+			requestDispatcher.forward(request, response);
+		}
+	}
+
+	public void init(FilterConfig config) throws ServletException {
+		LOG.debug("init");
+		this.errorPage = getInitParameter(config, INIT_PARAM_ERROR_PAGE);
+		LOG.debug("errorpage path: " + this.errorPage);
+	}
+
+	private String getInitParameter(FilterConfig config, String parameterName)
+			throws UnavailableException {
+		String value = config.getInitParameter(parameterName);
+		if (null == value) {
+			throw new UnavailableException("missing init parameter: "
+					+ parameterName);
+		}
+		return value;
+	}
+
+	public void destroy() {
+		LOG.debug("destroy");
+	}
+}
