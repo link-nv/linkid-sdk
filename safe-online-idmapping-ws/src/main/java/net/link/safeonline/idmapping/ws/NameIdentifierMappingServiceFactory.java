3030f60f5dee52e9a8949ea62081dcb5f304e570
fcorneli <fcorneli@cd7dd898-ab41-4057-98bc-1513970a52e4>
2007-10-05 13:15:17 +0000

    add: idmapping-ws project with WSDL/XSD for Name Identifier Mapping Service
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@686 cd7dd898-ab41-4057-98bc-1513970a52e4

---
+++ safe-online-idmapping-ws/src/main/java/net/link/safeonline/idmapping/ws/NameIdentifierMappingServiceFactory.java
@@ -0,0 +1,34 @@
+/*
+ * SafeOnline project.
+ * 
+ * Copyright 2006-2007 Lin.k N.V. All rights reserved.
+ * Lin.k N.V. proprietary/confidential. Use is subject to license terms.
+ */
+
+package net.link.safeonline.idmapping.ws;
+
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+
+import oasis.names.tc.saml._2_0.protocol.NameIdentifierMappingService;
+
+public class NameIdentifierMappingServiceFactory {
+
+	private NameIdentifierMappingServiceFactory() {
+		// empty
+	}
+
+	public static NameIdentifierMappingService newInstance() {
+		ClassLoader classLoader = Thread.currentThread()
+				.getContextClassLoader();
+		URL wsdlUrl = classLoader.getResource("saml-2.0-idmapping.wsdl");
+		if (null == wsdlUrl) {
+			throw new RuntimeException("SAML protocol WSDL not found");
+		}
+		NameIdentifierMappingService service = new NameIdentifierMappingService(
+				wsdlUrl, new QName("urn:oasis:names:tc:SAML:2.0:protocol",
+						"NameIdentifierMappingService"));
+		return service;
+	}
+}
---
+++ safe-online-idmapping-ws/src/test/java/test/unit/net/link/safeonline/idmapping/ws/NameIdentifierMappingServiceFactoryTest.java
@@ -0,0 +1,27 @@
+/*
+ * SafeOnline project.
+ * 
+ * Copyright 2006-2007 Lin.k N.V. All rights reserved.
+ * Lin.k N.V. proprietary/confidential. Use is subject to license terms.
+ */
+
+package test.unit.net.link.safeonline.idmapping.ws;
+
+import static org.junit.Assert.assertNotNull;
+import net.link.safeonline.idmapping.ws.NameIdentifierMappingServiceFactory;
+import oasis.names.tc.saml._2_0.protocol.NameIdentifierMappingService;
+
+import org.junit.Test;
+
+public class NameIdentifierMappingServiceFactoryTest {
+
+	@Test
+	public void testNewInstance() throws Exception {
+		// operate
+		NameIdentifierMappingService service = NameIdentifierMappingServiceFactory
+				.newInstance();
+
+		// verify
+		assertNotNull(service);
+	}
+}
