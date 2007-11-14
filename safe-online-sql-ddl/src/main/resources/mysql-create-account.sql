1cc466cace8b7e8f8d1406574fd5ed1a86e38bee
fcorneli <fcorneli@cd7dd898-ab41-4057-98bc-1513970a52e4>
2007-11-14 15:38:03 +0000

    add: some more install manual content
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@831 cd7dd898-ab41-4057-98bc-1513970a52e4

--- safe-online-sql-ddl/src/main/resources/mysql-create-account.sql
+++ safe-online-sql-ddl/src/main/resources/mysql-create-account.sql
@@ -1,10 +1,20 @@
 -- Account initialisation script for MySQL SafeOnline database
--- Usage: mysql -u root -p < mysql-create-account.sql
+-- <![CDATA[Usage: mysql -u root -p < mysql-create-account.sql ]]>
 USE mysql;
 DELETE FROM user WHERE User = 'safeonline';
 DELETE FROM db WHERE User = 'safeonline';
-INSERT INTO user (Host, User, Password) VALUES ('localhost', 'safeonline', PASSWORD('safeonline'));
-INSERT INTO user (Host, User, Password) VALUES ('%', 'safeonline', PASSWORD('safeonline'));
-INSERT INTO db (Host, Db, User, Select_priv, Insert_priv, Update_priv, Delete_priv, Create_priv, Drop_priv, Alter_priv, Index_priv) VALUES ('localhost', 'safeonline', 'safeonline', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y');
-INSERT INTO db (Host, Db, User, Select_priv, Insert_priv, Update_priv, Delete_priv, Create_priv, Drop_priv, Alter_priv, Index_priv) VALUES ('%', 'safeonline', 'safeonline', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y');
+INSERT INTO user (Host, User, Password)
+ VALUES ('localhost', 'safeonline', PASSWORD('safeonline'));
+INSERT INTO user (Host, User, Password) 
+ VALUES ('%', 'safeonline', PASSWORD('safeonline'));
+INSERT INTO db (Host, Db, User, Select_priv, Insert_priv, 
+ Update_priv, Delete_priv, Create_priv, Drop_priv, 
+ Alter_priv, Index_priv) 
+ VALUES ('localhost', 'safeonline', 'safeonline', 
+ 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y');
+INSERT INTO db (Host, Db, User, Select_priv, Insert_priv,
+ Update_priv, Delete_priv, Create_priv, Drop_priv,
+ Alter_priv, Index_priv) 
+ VALUES ('%', 'safeonline', 'safeonline', 
+ 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y');
 FLUSH PRIVILEGES;
