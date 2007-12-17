c8020359032168c6d113c3a88a5a851ee85f0deb
mbillemo <mbillemo@cd7dd898-ab41-4057-98bc-1513970a52e4>
2007-12-17 09:57:38 +0000

    fix: xml-maven-plugin moved from dependencymanagement -> pluginmanagement
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@928 cd7dd898-ab41-4057-98bc-1513970a52e4

--- safe-online-service/src/main/java/net/link/safeonline/jaas/SafeOnlineLoginModule.java
+++ safe-online-service/src/main/java/net/link/safeonline/jaas/SafeOnlineLoginModule.java
@@ -31,179 +31,174 @@ import org.jboss.security.SimpleGroup;
 import org.jboss.security.SimplePrincipal;
 
 /**
- * JAAS login module that performs authentication and authorization used by the
- * SafeOnline core security domain.
+ * JAAS login module that performs authentication and authorization used by the SafeOnline core security domain.
  * 
  * @author fcorneli
- * 
  */
 public class SafeOnlineLoginModule implements LoginModule {
 
-	private static final Log LOG = LogFactory
-			.getLog(SafeOnlineLoginModule.class);
+    private static final Log   LOG                                     = LogFactory.getLog( SafeOnlineLoginModule.class );
 
-	private Subject subject;
+    private Subject            subject;
 
-	private CallbackHandler callbackHandler;
+    private CallbackHandler    callbackHandler;
 
-	public static final String OPTION_AUTHORIZATION_SERVICE_JNDI_NAME = "authorizationServiceJndiName";
+    public static final String OPTION_AUTHORIZATION_SERVICE_JNDI_NAME  = "authorizationServiceJndiName";
 
-	public static final String DEFAULT_AUTHORIZATION_SERVICE_JNDI_NAME = "SafeOnline/AuthorizationServiceBean/local";
+    public static final String DEFAULT_AUTHORIZATION_SERVICE_JNDI_NAME = "SafeOnline/AuthorizationServiceBean/local";
 
-	private String authorizationServiceJndiName;
+    private String             authorizationServiceJndiName;
 
-	private Principal authenticatedPrincipal;
+    private Principal          authenticatedPrincipal;
 
-	private Set<String> roles;
+    private Set<String>        roles;
 
-	public boolean abort() {
-		LOG.debug("abort");
+    public boolean abort() {
 
-		this.authenticatedPrincipal = null;
-		this.roles = null;
+        LOG.debug( "abort" );
 
-		return true;
-	}
+        this.authenticatedPrincipal = null;
+        this.roles = null;
 
-	public boolean commit() throws LoginException {
-		LOG.debug("commit");
+        return true;
+    }
 
-		Set<Principal> principals = this.subject.getPrincipals();
-		if (null == this.authenticatedPrincipal)
-			throw new LoginException(
-					"authenticated principal should be not null");
-		// authenticate
-		principals.add(this.authenticatedPrincipal);
+    public boolean commit() throws LoginException {
 
-		// make JBoss happy
-		Group callerPrincipalGroup = getGroup("CallerPrincipal", principals);
-		callerPrincipalGroup.addMember(this.authenticatedPrincipal);
+        LOG.debug( "commit" );
 
-		// authorize
-		Group rolesGroup = getGroup("Roles", principals);
+        Set<Principal> principals = this.subject.getPrincipals();
+        if (null == this.authenticatedPrincipal)
+            throw new LoginException( "authenticated principal should be not null" );
+        // authenticate
+        principals.add( this.authenticatedPrincipal );
 
-		if (null == this.roles)
-			return true;
+        // make JBoss happy
+        Group callerPrincipalGroup = getGroup( "CallerPrincipal", principals );
+        callerPrincipalGroup.addMember( this.authenticatedPrincipal );
 
-		for (String role : this.roles)
-			rolesGroup.addMember(new SimplePrincipal(role));
+        // authorize
+        Group rolesGroup = getGroup( "Roles", principals );
 
-		LOG.debug("committed");
+        if (null == this.roles)
+            return true;
 
-		return true;
-	}
+        for (String role : this.roles)
+            rolesGroup.addMember( new SimplePrincipal( role ) );
 
-	private String getOptionValue(Map<?, ?> options, String optionName,
-			String defaultOptionValue) {
-		String optionValue = (String) options.get(optionName);
-		if (null == optionValue) {
-			optionValue = defaultOptionValue;
-			LOG.debug("using default option value for " + optionName + " = "
-					+ defaultOptionValue);
-		}
-		return optionValue;
-	}
+        LOG.debug( "committed" );
 
-	public void initialize(Subject newSubject,
-			CallbackHandler newCallbackHandler, @SuppressWarnings("unchecked")
-			Map sharedState, @SuppressWarnings("unchecked")
-			Map options) {
-		LOG.debug("initialize");
+        return true;
+    }
 
-		this.authorizationServiceJndiName = getOptionValue(options,
-				OPTION_AUTHORIZATION_SERVICE_JNDI_NAME,
-				DEFAULT_AUTHORIZATION_SERVICE_JNDI_NAME);
+    private String getOptionValue(Map<?, ?> options, String optionName, String defaultOptionValue) {
 
-		this.subject = newSubject;
-		this.callbackHandler = newCallbackHandler;
-		LOG.debug("subject class: " + this.subject.getClass().getName());
-		LOG.debug("callback handler class: "
-				+ this.callbackHandler.getClass().getName());
-	}
+        String optionValue = (String) options.get( optionName );
+        if (null == optionValue) {
+            optionValue = defaultOptionValue;
+            LOG.debug( "using default option value for " + optionName + " = " + defaultOptionValue );
+        }
+        return optionValue;
+    }
 
-	private Group getGroup(String groupName, Set<Principal> principals) {
-		Iterator<?> iter = principals.iterator();
-		while (iter.hasNext()) {
-			Object next = iter.next();
-			if (next instanceof Group == false)
-				continue;
-			Group group = (Group) next;
-			if (group.getName().equals(groupName))
-				return group;
-		}
-		// If we did not find a group create one
-		Group group = new SimpleGroup(groupName);
-		principals.add(group);
-		return group;
-	}
+    public void initialize(Subject newSubject, CallbackHandler newCallbackHandler, @SuppressWarnings("unchecked")
+    Map sharedState, @SuppressWarnings("unchecked")
+    Map options) {
 
-	public boolean login() throws LoginException {
-		LOG.debug("login");
-		// retrieve credentials
-		NameCallback nameCallback = new NameCallback("username");
-		// TODO: we should verify a SAML assertion here
-		Callback[] callbacks = new Callback[] { nameCallback };
+        LOG.debug( "initialize" );
 
-		try {
-			this.callbackHandler.handle(callbacks);
-		} catch (IOException e) {
-			String msg = "IO error: " + e.getMessage();
-			LOG.error(msg);
-			throw new LoginException(msg);
-		} catch (UnsupportedCallbackException e) {
-			String msg = "unsupported callback: " + e.getMessage();
-			LOG.error(msg);
-			throw new LoginException(msg);
-		}
+        this.authorizationServiceJndiName = getOptionValue( options, OPTION_AUTHORIZATION_SERVICE_JNDI_NAME,
+                DEFAULT_AUTHORIZATION_SERVICE_JNDI_NAME );
 
-		String username = nameCallback.getName();
-		LOG.debug("username: " + username);
-		// if (null == username) {
-		// throw new LoginException("username is null");
-		// }
+        this.subject = newSubject;
+        this.callbackHandler = newCallbackHandler;
+        LOG.debug( "subject class: " + this.subject.getClass().getName() );
+        LOG.debug( "callback handler class: " + this.callbackHandler.getClass().getName() );
+    }
 
-		// authenticate
-		// TODO: authenticate here again via SAML assertion
-		LOG.debug("authenticated");
+    private Group getGroup(String groupName, Set<Principal> principals) {
 
-		this.authenticatedPrincipal = new SimplePrincipal(username);
+        Iterator<?> iter = principals.iterator();
+        while (iter.hasNext()) {
+            Object next = iter.next();
+            if (next instanceof Group == false)
+                continue;
+            Group group = (Group) next;
+            if (group.getName().equals( groupName ))
+                return group;
+        }
+        // If we did not find a group create one
+        Group group = new SimpleGroup( groupName );
+        principals.add( group );
+        return group;
+    }
 
-		// authorize
-		AuthorizationService authorizationService = getAuthorizationService();
+    public boolean login() throws LoginException {
 
-		this.roles = authorizationService.getRoles(username);
+        LOG.debug( "login" );
+        // retrieve credentials
+        NameCallback nameCallback = new NameCallback( "username" );
+        // TODO: we should verify a SAML assertion here
+        Callback[] callbacks = new Callback[] { nameCallback };
 
-		return true;
-	}
+        try {
+            this.callbackHandler.handle( callbacks );
+        } catch (IOException e) {
+            String msg = "IO error: " + e.getMessage();
+            LOG.error( msg );
+            throw new LoginException( msg );
+        } catch (UnsupportedCallbackException e) {
+            String msg = "unsupported callback: " + e.getMessage();
+            LOG.error( msg );
+            throw new LoginException( msg );
+        }
 
-	private AuthorizationService getAuthorizationService()
-			throws LoginException {
-		try {
-			AuthorizationService authorizationService = EjbUtils.getEJB(
-					this.authorizationServiceJndiName,
-					AuthorizationService.class);
-			return authorizationService;
-		} catch (RuntimeException e) {
-			throw new LoginException("JNDI lookup error: " + e.getMessage());
-		}
-	}
+        String username = nameCallback.getName();
+        LOG.debug( "username: " + username );
+        if (null == username)
+            throw new LoginException( "username is null" );
 
-	public boolean logout() throws LoginException {
-		LOG.debug("logout");
-		Set<Principal> principals = this.subject.getPrincipals();
-		if (null == this.authenticatedPrincipal)
-			throw new LoginException(
-					"authenticated principal should not be null");
-		boolean result = principals.remove(this.authenticatedPrincipal);
-		if (!result)
-			throw new LoginException("could not remove authenticated principal");
-		/*
-		 * Despite the fact that JBoss AbstractServerLoginModule is not removing
-		 * the roles on the subject, we clear here all data on the subject.
-		 */
-		this.subject.getPrincipals().clear();
-		this.subject.getPublicCredentials().clear();
-		this.subject.getPrivateCredentials().clear();
-		return true;
-	}
+        // authenticate
+        // TODO: authenticate here again via SAML assertion
+        LOG.debug( "authenticated" );
+
+        this.authenticatedPrincipal = new SimplePrincipal( username );
+
+        // authorize
+        AuthorizationService authorizationService = getAuthorizationService();
+
+        this.roles = authorizationService.getRoles( username );
+
+        return true;
+    }
+
+    private AuthorizationService getAuthorizationService() throws LoginException {
+
+        try {
+            AuthorizationService authorizationService = EjbUtils.getEJB( this.authorizationServiceJndiName,
+                    AuthorizationService.class );
+            return authorizationService;
+        } catch (RuntimeException e) {
+            throw new LoginException( "JNDI lookup error: " + e.getMessage() );
+        }
+    }
+
+    public boolean logout() throws LoginException {
+
+        LOG.debug( "logout" );
+        Set<Principal> principals = this.subject.getPrincipals();
+        if (null == this.authenticatedPrincipal)
+            throw new LoginException( "authenticated principal should not be null" );
+        boolean result = principals.remove( this.authenticatedPrincipal );
+        if (!result)
+            throw new LoginException( "could not remove authenticated principal" );
+        /*
+         * Despite the fact that JBoss AbstractServerLoginModule is not removing
+         * the roles on the subject, we clear here all data on the subject.
+         */
+        this.subject.getPrincipals().clear();
+        this.subject.getPublicCredentials().clear();
+        this.subject.getPrivateCredentials().clear();
+        return true;
+    }
 }
