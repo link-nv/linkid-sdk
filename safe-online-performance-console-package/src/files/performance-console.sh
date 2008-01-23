58b610cf621a9eed352313f6e6deda554fa904df
mbillemo <mbillemo@cd7dd898-ab41-4057-98bc-1513970a52e4>
2008-01-23 10:58:09 +0000

    fix: console-swing: make scope of commons-logging runtime instead of test so it'll be added to the executable jar's classpath (useful for console-package). fix: scripts NEED a shebang.  using #!/bin/sh.  also adding a cd "${0%/*}" so that when invoking the script from another directory it'll still find the jars. fix: console-package: make scope of commons-logging runtime instead of test so it'll be added to the zip package.
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@1013 cd7dd898-ab41-4057-98bc-1513970a52e4

--- safe-online-performance-console-package/src/files/performance-console.sh
+++ safe-online-performance-console-package/src/files/performance-console.sh
@@ -1,2 +1,6 @@
-echo "Starting the Performance Console..."
+#!/bin/sh
+
+echo "Starting the Performance Console ..."
+
+cd "${0%/*}"
 java -jar safe-online-performance-console-swing-1.0-SNAPSHOT.jar
