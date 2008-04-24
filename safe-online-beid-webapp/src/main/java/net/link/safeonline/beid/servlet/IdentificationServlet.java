8b557b55e83c9b0fdd1ff262a6fc065d41f04ca6
fcorneli <fcorneli@cd7dd898-ab41-4057-98bc-1513970a52e4>
2008-04-24 14:15:29 +0000

    add: identification entry servlet + info page
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@1208 cd7dd898-ab41-4057-98bc-1513970a52e4

---
+++ safe-online-beid-webapp/src/main/java/net/link/safeonline/beid/servlet/IdentificationServlet.java
@@ -0,0 +1,41 @@
+/*
+ * SafeOnline project.
+ * 
+ * Copyright 2006-2008 Lin.k N.V. All rights reserved.
+ * Lin.k N.V. proprietary/confidential. Use is subject to license terms.
+ */
+
+package net.link.safeonline.beid.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class IdentificationServlet extends HttpServlet {
+
+	private static final Log LOG = LogFactory
+			.getLog(IdentificationServlet.class);
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String targetParameter = request.getParameter("target");
+		if (null == targetParameter) {
+			response.sendRedirect("identification.seam");
+		} else {
+			LOG.debug("target: " + targetParameter);
+			HttpSession session = request.getSession();
+			session.setAttribute("target", targetParameter);
+			response.sendRedirect("identification-pcsc.seam");
+		}
+	}
+}
