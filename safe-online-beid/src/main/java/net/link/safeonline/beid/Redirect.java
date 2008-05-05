3528d5649d8373fa65ed1e4a7b73edd1de2259bd
wvdhaute <wvdhaute@cd7dd898-ab41-4057-98bc-1513970a52e4>
2008-05-05 09:04:07 +0000

    fix: beid redirect page backing beans, forgot to commit
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@1245 cd7dd898-ab41-4057-98bc-1513970a52e4

---
+++ safe-online-beid/src/main/java/net/link/safeonline/beid/Redirect.java
@@ -0,0 +1,25 @@
+/*
+ * SafeOnline project.
+ * 
+ * Copyright 2006-2008 Lin.k N.V. All rights reserved.
+ * Lin.k N.V. proprietary/confidential. Use is subject to license terms.
+ */
+
+package net.link.safeonline.beid;
+
+import javax.ejb.Local;
+
+@Local
+public interface Redirect {
+	/*
+	 * Accessor
+	 */
+	String getRedirectUrl();
+
+	/*
+	 * Lifecycle callbacks
+	 */
+	void initCallback();
+
+	void destroyCallback();
+}
