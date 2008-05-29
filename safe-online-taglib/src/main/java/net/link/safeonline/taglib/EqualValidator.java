7808907001d907e511015e46bb8b874b6b8b8e19
mbillemo <mbillemo@cd7dd898-ab41-4057-98bc-1513970a52e4>
2008-05-29 14:59:00 +0000

    fix: handle nulls properly.
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@1353 cd7dd898-ab41-4057-98bc-1513970a52e4

--- safe-online-taglib/src/main/java/net/link/safeonline/taglib/EqualValidator.java
+++ safe-online-taglib/src/main/java/net/link/safeonline/taglib/EqualValidator.java
@@ -14,27 +14,31 @@ import org.apache.commons.logging.LogFactory;
 
 public class EqualValidator implements Validator {
 
-	private static final Log LOG = LogFactory.getLog(EqualValidator.class);
+    private static final Log LOG = LogFactory.getLog(EqualValidator.class);
 
-	public void validate(FacesContext context, UIComponent component,
-			Object value) throws ValidatorException {
-		String confirm = (String) value;
 
-		String textId = (String) component.getAttributes().get("for");
+    public void validate(FacesContext context, UIComponent component,
+            Object value) throws ValidatorException {
 
-		UIInput input = (UIInput) context.getViewRoot().findComponent(textId);
+        String confirm = (String) value;
 
-		String text = (String) input.getValue();
+        String textId = (String) component.getAttributes().get("for");
 
-		if (!text.equals(confirm)) {
-			ResourceBundle messages = TaglibUtil
-					.getResourceBundle(context);
-			FacesMessage facesMessage = new FacesMessage(messages
-					.getString("notEqual"));
-			LOG.error("fields are not equal");
-			throw new ValidatorException(facesMessage);
-		}
+        UIInput input = (UIInput) context.getViewRoot().findComponent(textId);
 
-	}
+        String text = (String) input.getValue();
+
+        if (text == null) {
+            if (confirm == null)
+                return;
+        } else if (text.equals(confirm))
+            return;
+
+        ResourceBundle messages = TaglibUtil.getResourceBundle(context);
+        FacesMessage facesMessage = new FacesMessage(messages
+                .getString("notEqual"));
+        LOG.error("fields are not equal");
+        throw new ValidatorException(facesMessage);
+    }
 
 }
