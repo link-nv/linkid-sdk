1430020b2d9825f6bafc8a9b3150e770d5612d54
mbillemo <mbillemo@cd7dd898-ab41-4057-98bc-1513970a52e4>
2008-06-11 11:20:25 +0000

    fix: STS validate() now also requires a trustdomain.
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@1405 cd7dd898-ab41-4057-98bc-1513970a52e4

--- safe-online-performance-scenario-base/src/main/java/net/link/safeonline/performance/drivers/StsDriver.java
+++ safe-online-performance-scenario-base/src/main/java/net/link/safeonline/performance/drivers/StsDriver.java
@@ -12,6 +12,7 @@ import java.security.cert.X509Certificate;
 import net.link.safeonline.performance.entity.ExecutionEntity;
 import net.link.safeonline.performance.entity.ScenarioTimingEntity;
 import net.link.safeonline.sdk.ws.sts.SecurityTokenServiceClientImpl;
+import net.link.safeonline.sdk.ws.sts.TrustDomainType;
 
 import org.w3c.dom.Element;
 
@@ -27,54 +28,58 @@ import org.w3c.dom.Element;
  */
 public class StsDriver extends ProfileDriver {
 
-	public static final String NAME = "Security Token Service Driver";
-	public static final String DESCRIPTION = "<b>Security Token Service Driver:</b><br>"
-			+ "Checks the validity of a SAML token and causes an exception if it is not valid.";
+    public static final String NAME        = "Security Token Service Driver";
+    public static final String DESCRIPTION = "<b>Security Token Service Driver:</b><br>"
+                                                   + "Checks the validity of a SAML token and causes an exception if it is not valid.";
 
-	public StsDriver(ExecutionEntity execution, ScenarioTimingEntity agentTime) {
 
-		super(NAME, execution, agentTime);
-	}
+    public StsDriver(ExecutionEntity execution, ScenarioTimingEntity agentTime) {
 
-	/**
-	 * Validate the given SAML token.
-	 * 
-	 * @param applicationKey
-	 *            The certificate of the application making the request. This
-	 *            identifies the application and gives the request the
-	 *            application's authority.
-	 * @param token
-	 *            The SAML token that needs to be validated.
-	 */
-	public void validate(PrivateKeyEntry applicationKey, Element token) {
+        super(NAME, execution, agentTime);
+    }
 
-		if (!(applicationKey.getCertificate() instanceof X509Certificate))
-			throw new IllegalArgumentException(
-					"The certificate in the keystore needs to be of X509 format.");
+    /**
+     * Validate the given SAML token.
+     * 
+     * @param applicationKey
+     *            The certificate of the application making the request. This
+     *            identifies the application and gives the request the
+     *            application's authority.
+     * @param token
+     *            The SAML token that needs to be validated.
+     * @param trustDomain
+     *            The trusted party that released the certificate of the token.
+     */
+    public void validate(PrivateKeyEntry applicationKey, Element token,
+            TrustDomainType trustDomain) {
 
-		try {
-			SecurityTokenServiceClientImpl service = new SecurityTokenServiceClientImpl(
-					getHost(), (X509Certificate) applicationKey
-							.getCertificate(), applicationKey.getPrivateKey());
+        if (!(applicationKey.getCertificate() instanceof X509Certificate))
+            throw new IllegalArgumentException(
+                    "The certificate in the keystore needs to be of X509 format.");
 
-			try {
-				service.validate(token);
-			} finally {
-				report(service);
-			}
-		}
+        try {
+            SecurityTokenServiceClientImpl service = new SecurityTokenServiceClientImpl(
+                    getHost(), (X509Certificate) applicationKey
+                            .getCertificate(), applicationKey.getPrivateKey());
 
-		catch (Throwable error) {
-			throw report(error);
-		}
-	}
+            try {
+                service.validate(token, trustDomain);
+            } finally {
+                report(service);
+            }
+        }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public String getDescription() {
+        catch (Throwable error) {
+            throw report(error);
+        }
+    }
 
-		return DESCRIPTION;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getDescription() {
+
+        return DESCRIPTION;
+    }
 }
