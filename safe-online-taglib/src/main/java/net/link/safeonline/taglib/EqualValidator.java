641dd5945b24138e07aebe1f95f605c371343dc8
wvdhaute <wvdhaute@cd7dd898-ab41-4057-98bc-1513970a52e4>
2008-07-08 13:03:40 +0000

    fix: EqualValidator has attribute for custom messages
    git-svn-id: svn+ssh://buildserver.lin-k.net/scm/svn/products/safe-online/trunk@1486 cd7dd898-ab41-4057-98bc-1513970a52e4

--- safe-online-taglib/src/main/java/net/link/safeonline/taglib/EqualValidator.java
+++ safe-online-taglib/src/main/java/net/link/safeonline/taglib/EqualValidator.java
@@ -14,30 +14,35 @@ import org.apache.commons.logging.LogFactory;
 
 public class EqualValidator implements Validator {
 
-    private static final Log LOG = LogFactory.getLog(EqualValidator.class);
+	private static final Log LOG = LogFactory.getLog(EqualValidator.class);
 
+	public void validate(FacesContext context, UIComponent component,
+			Object value) throws ValidatorException {
 
-    public void validate(FacesContext context, UIComponent component,
-            Object value) throws ValidatorException {
+		String confirm = (String) value;
 
-        String confirm = (String) value;
+		String textId = (String) component.getAttributes().get("for");
 
-        String textId = (String) component.getAttributes().get("for");
+		String message = (String) component.getAttributes().get("message");
 
-        UIInput input = (UIInput) context.getViewRoot().findComponent(textId);
+		UIInput input = (UIInput) context.getViewRoot().findComponent(textId);
 
-        String text = (String) input.getValue();
+		String text = (String) input.getValue();
 
-        if (text == null || confirm == null)
-            return;
-        else if (text.equals(confirm))
-            return;
+		if (text == null || confirm == null)
+			return;
+		else if (text.equals(confirm))
+			return;
 
-        ResourceBundle messages = TaglibUtil.getResourceBundle(context);
-        FacesMessage facesMessage = new FacesMessage(messages
-                .getString("notEqual"));
-        LOG.error("fields are not equal");
-        throw new ValidatorException(facesMessage);
-    }
+		FacesMessage facesMessage;
+		if (null == message) {
+			ResourceBundle messages = TaglibUtil.getResourceBundle(context);
+			facesMessage = new FacesMessage(messages.getString("notEqual"));
+		} else {
+			facesMessage = new FacesMessage(message);
+		}
+		LOG.error("fields are not equal");
+		throw new ValidatorException(facesMessage);
+	}
 
 }
