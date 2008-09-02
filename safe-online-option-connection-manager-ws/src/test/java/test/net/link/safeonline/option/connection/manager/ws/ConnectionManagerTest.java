6a7fc687ef8c00197f40acf71e4f11efdad439cc
Dieter Houthooft <dieter@lin-k.net>
2008-09-02 11:08:35 +0200

    Added WS artifact for the connection manager

---
+++ safe-online-option-connection-manager-ws/src/test/java/test/net/link/safeonline/option/connection/manager/ws/ConnectionManagerTest.java
@@ -0,0 +1,45 @@
+package test.net.link.safeonline.option.connection.manager.ws;
+
+import java.net.URL;
+
+import javax.jws.WebService;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Endpoint;
+
+import net.link.safeonline.option.connection.manager.ws.generated.ConnectionManager;
+import net.link.safeonline.option.connection.manager.ws.generated.ConnectionManagerService;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConnectionManagerTest {
+
+	public static final String URL = "http://localhost:8123/connection-manager";
+
+	@Test
+	public void testConnectionManagerWebService() throws Exception {
+
+		Endpoint.publish(URL, new ConnectionManagerImpl());
+
+		ConnectionManagerService service = new ConnectionManagerService(
+				new URL(URL + "?wsdl"),
+				new QName(
+						"http://ws.manager.connection.option.safeonline.link.net/",
+						"ConnectionManagerService"));
+		ConnectionManager port = service.getConnectionManagerPort();
+		assertEquals(ConnectionManagerImpl.IMEI, port.getIMEI());
+	}
+
+	@WebService(portName = "ConnectionManagerPort", targetNamespace = "http://ws.manager.connection.option.safeonline.link.net/", serviceName = "ConnectionManagerService")
+	public class ConnectionManagerImpl implements ConnectionManager {
+
+		public static final String IMEI = "123";
+
+		public String getIMEI() {
+			return IMEI;
+		}
+
+	}
+
+}
