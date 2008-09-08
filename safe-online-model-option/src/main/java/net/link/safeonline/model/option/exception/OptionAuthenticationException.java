c0c5a1ea4a113f447a9757ce55afb43934ac0557
Dieter Houthooft <dieter@lin-k.net>
2008-09-08 19:36:30 +0200

    Added model-option artifact
    Supports only one option device per user

---
+++ safe-online-model-option/src/main/java/net/link/safeonline/model/option/exception/OptionAuthenticationException.java
@@ -0,0 +1,33 @@
+/*
+ * SafeOnline project.
+ * 
+ * Copyright 2006-2008 Lin.k N.V. All rights reserved.
+ * Lin.k N.V. proprietary/confidential. Use is subject to license terms.
+ */
+package net.link.safeonline.model.option.exception;
+
+import javax.ejb.ApplicationException;
+
+import net.link.safeonline.authentication.exception.SafeOnlineException;
+
+
+/**
+ * <h2>{@link OptionAuthenticationException}<br>
+ * <sub>[in short] (TODO).</sub></h2>
+ * 
+ * <p>
+ * [description / usage].
+ * </p>
+ * 
+ * <p>
+ * <i>Sep 8, 2008</i>
+ * </p>
+ * 
+ * @author dhouthoo
+ */
+@ApplicationException(rollback = true)
+public class OptionAuthenticationException extends SafeOnlineException {
+
+    private static final long serialVersionUID = 1L;
+
+}
