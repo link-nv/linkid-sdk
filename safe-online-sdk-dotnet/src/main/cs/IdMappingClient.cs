4ea414dc28f6a4d9c3e168ed0c64c90ed28a3dc4
Wim Vandenhaute <wim@lin-k.net>
2008-10-09 15:15:16 +0200

    merge

--- safe-online-sdk-dotnet/src/main/cs/IdMappingClient.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace safe_online_sdk_dotnet
-{
-	public interface IdMappingClient
-	{
-		string getUserId(String username);
-	}
-}
--- safe-online-sdk-dotnet/src/main/cs/STSClient.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace safe_online_sdk_dotnet
-{
-	public interface STSClient
-	{
-		bool validateAuthnResponse(string authnResponse);
-	}
-}
