21941c3b62f1ae256d72e0f5e92a776440b30936
Dieter Houthooft <dieter@lin-k.net>
2009-02-18 10:51:51 +0100

    Added clickatell template and renamed bundle artifacts

---
+++ safe-online-sms-template-bundle/src/main/java/net/link/safeonline/osgi/sms/template/TemplateSmsServiceActivator.java
@@ -0,0 +1,43 @@
+/*
+ * SafeOnline project.
+ * 
+ * Copyright 2006-2008 Lin.k N.V. All rights reserved.
+ * Lin.k N.V. proprietary/confidential. Use is subject to license terms.
+ */
+package net.link.safeonline.osgi.sms.template;
+
+import net.link.safeonline.osgi.sms.SmsService;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+
+public class TemplateSmsServiceActivator implements BundleActivator {
+
+    ServiceRegistration templateSmsServiceRegistration;
+
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+     */
+    public void start(BundleContext context)
+            throws Exception {
+
+        TemplateSmsServiceFactory smsServiceFactory = new TemplateSmsServiceFactory();
+        templateSmsServiceRegistration = context.registerService(SmsService.class.getName(), smsServiceFactory, null);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+     */
+    public void stop(BundleContext context)
+            throws Exception {
+
+        templateSmsServiceRegistration.unregister();
+    }
+}
