8a76db0409122a99cc53d3f5d7c43fa784e7325b
Wim Vandenhaute <wim@lin-k.net>
2009-03-05 14:16:35 +0100

    mod: identity unavailable page
    mod: SOS-431
    fix: encap webapp unit test

--- safe-online-encap-webapp/src/test/java/test/unit/net/link/safeonline/encap/webapp/AuthenticationPageTest.java
+++ safe-online-encap-webapp/src/test/java/test/unit/net/link/safeonline/encap/webapp/AuthenticationPageTest.java
@@ -91,7 +91,7 @@ public class AuthenticationPageTest {
         authenticationContext.setApplicationFriendlyName(TEST_APPLICATION);
         ProtocolContext protocolContext = ProtocolContext.getProtocolContext(wicket.getServletSession());
         protocolContext.setSubject(TEST_USERID);
-        protocolContext.setAttributeId(TEST_MOBILE);
+        protocolContext.setAttribute(TEST_MOBILE);
 
         // Check whether our mount point sends us to the authentication page.
         switch (goal) {
