7895b0ff5e9146518cd4ddf8312adf664e5fe851
Maarten Billemont <lhunath@gmail.com>
2009-03-09 14:30:11 +0100

    Manage transaction for create config group: if it fails, it's because another thread already did the create; in that case, we just re-read the data because it should be there now.
    upd: Updated initdb script for Oracle so that it properly cleans up the oracle database (drops and recreates tables).

--- safe-online-model/src/main/java/net/link/safeonline/config/dao/bean/ConfigGroupDAOBean.java
+++ safe-online-model/src/main/java/net/link/safeonline/config/dao/bean/ConfigGroupDAOBean.java
@@ -11,6 +11,8 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -38,6 +40,7 @@ public class ConfigGroupDAOBean implements ConfigGroupDAO {
         queryObject = QueryObjectFactory.createQueryObject(entityManager, ConfigGroupEntity.QueryInterface.class);
     }
 
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
     public ConfigGroupEntity addConfigGroup(String name) {
 
         ConfigGroupEntity configGroup = new ConfigGroupEntity(name);
