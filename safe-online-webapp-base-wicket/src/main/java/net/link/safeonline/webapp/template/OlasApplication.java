4a9155639edfbc7e1bc91b63b6654f58fbdbeb31
Wim Vandenhaute <wim@lin-k.net>
2009-03-13 09:37:20 +0100

    mod: changed error page to page unavailable page

--- safe-online-webapp-base-wicket/src/main/java/net/link/safeonline/webapp/template/OlasApplication.java
+++ safe-online-webapp-base-wicket/src/main/java/net/link/safeonline/webapp/template/OlasApplication.java
@@ -2,6 +2,7 @@ package net.link.safeonline.webapp.template;
 
 import net.link.safeonline.webapp.common.ErrorPage;
 import net.link.safeonline.webapp.common.MyRequestCycle;
+import net.link.safeonline.webapp.common.NotFoundPage;
 import net.link.safeonline.webapp.common.ProtocolErrorPage;
 import net.link.safeonline.wicket.tools.CustomStringResourceLoader;
 import net.link.safeonline.wicket.tools.WicketUtil;
@@ -29,6 +30,7 @@ public abstract class OlasApplication extends WebApplication {
 
         mountBookmarkablePage("protocol-error", ProtocolErrorPage.class);
         mountBookmarkablePage("error", ErrorPage.class);
+        mountBookmarkablePage("not-found", NotFoundPage.class);
 
         getResourceSettings().addStringResourceLoader(new CustomStringResourceLoader("WEB-INF/classes/messages.webapp"));
     }
