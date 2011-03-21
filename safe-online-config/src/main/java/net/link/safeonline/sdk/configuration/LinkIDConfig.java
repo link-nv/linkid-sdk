2cbfa7c650794e5f98f6ef17781c3c73b83a47c1
Wim Vandenhaute <wim@lin-k.net>
2011-03-21 17:04:32 +0100

    fix: classloading issue with device/core ears, lisu approach + globalclassloader fix

--- safe-online-config/src/main/java/net/link/safeonline/sdk/configuration/LinkIDConfig.java
+++ safe-online-config/src/main/java/net/link/safeonline/sdk/configuration/LinkIDConfig.java
@@ -3,13 +3,11 @@ package net.link.safeonline.sdk.configuration;
 import java.util.Locale;
 import net.link.util.config.Config;
 
+
 /**
- * <h2>{@link LinkIDConfig}<br>
- * <sub>[in short] (TODO).</sub></h2>
+ * <h2>{@link LinkIDConfig}<br> <sub>[in short] (TODO).</sub></h2>
  *
- * <p>
- * <i>09 15, 2010</i>
- * </p>
+ * <p> <i>09 15, 2010</i> </p>
  *
  * @author lhunath
  */
@@ -26,11 +24,9 @@ public interface LinkIDConfig {
      *
      * <i>[required, default: /entry]</i>
      *
-     * <p>
-     * We go here when the user begins an authentication from our application.
-     * </p>
+     * <p> We go here when the user begins an authentication from our application. </p>
      */
-    @Config.Property(required = true, unset = "/entry")
+    @Config.Property(required = true, unset = "/core/entry")
     String authPath();
 
     /**
@@ -38,9 +34,7 @@ public interface LinkIDConfig {
      *
      * <i>[required, default: /logout]</i>
      *
-     * <p>
-     * We go here when the user begins a logout from our application.
-     * </p>
+     * <p> We go here when the user begins a logout from our application. </p>
      */
     @Config.Property(required = true, unset = "/logoutentry")
     String logoutPath();
@@ -50,23 +44,19 @@ public interface LinkIDConfig {
      *
      * <i>[required, default: /logoutexit]</i>
      *
-     * <p>
-     * We go here after linkID-auth asked us to clean our session up following a logout request that was initiated from another application
-     * in our application's SSO pool.  This response allows linkID to find out whether we successfully logged the user out of the application's SSO session.
-     * </p>
+     * <p> We go here after linkID-auth asked us to clean our session up following a logout request that was initiated from another
+     * application in our application's SSO pool.  This response allows linkID to find out whether we successfully logged the user out of
+     * the application's SSO session. </p>
      */
     @Config.Property(required = true, unset = "/logoutexit")
     String logoutExitPath();
-
 
     /**
      * Specifies a name of a CSS theme that changes the looks of the linkID application.
      *
      * <i>[optional, default: The application's default theme configured at the linkID node]</i>
      *
-     * <p>
-     * Accepted values are names of themes configured by the operator of the linkID node you'll be using.
-     * </p>
+     * <p> Accepted values are names of themes configured by the operator of the linkID node you'll be using. </p>
      */
     @Config.Property(required = false)
     String theme();
