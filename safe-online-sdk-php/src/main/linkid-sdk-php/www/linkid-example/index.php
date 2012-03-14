2b6396d022088e18a5c491bb2e46824a2b1ce73e
Wim Vandenhaute <wim.vandenhaute@gmail.com>
2012-03-14 10:40:36 +0100

    Add PHP SDK + manual

---
+++ safe-online-sdk-php/src/main/linkid-sdk-php/www/linkid-example/index.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once('../_include.php');
+
+        $as = new SimpleSAML_Auth_Simple('linkid-example');
+
+?>
+
+<html>
+    <head>
+
+    <!-- linkID login js for running the authentication process in framed/popup mode -->
+    <script type="text/javascript" id="linkid-login-script" src="https://demo.linkid.be:443/linkid-auth/resources/common/js/linkid.login-min.js"></script>
+
+    <h1>Example webapp</h1>
+    </head>
+
+    <body>
+
+
+        <?php 
+        
+        if ($as->isAuthenticated()) {
+
+            $authDataArray = $as->getAuthDataArray();
+            $userId        = $authDataArray['saml:sp:NameID']['Value'];
+            $attributes    = $as->getAttributes();
+        ?>
+
+        <p>
+            <h2>Hello <?php print $authDataArray['saml:sp:NameID']['Value'] ?>  )!</h2>
+        </p>
+
+        <h3>Attributes</h3>
+        <p>
+          <?php print_r($as->getAttributes()); ?>
+        </p>
+
+        <!--
+
+        Example accessing a compound member attribute:
+
+        print "{$attributes['device.beid'][0]['device.beid.givenName'][0]}";
+
+        -->    
+
+        <a href="./logout.php">Logout</a>
+
+        <?php
+        
+        } else { 
+        
+        ?>
+
+        <p>
+            <!-- The css class is read by the linkid.login.js for running the authentication process in a framed/popup mode -->
+            <a href="./login.php" class="linkid-login">Login</a>
+        </p>
+
+        <?php } ?>
+
+    </body>
+</html>
+
---
+++ safe-online-sdk-php/src/main/linkid-sdk-php/www/linkid-example/login.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once('../_include.php');
+
+        $as = new SimpleSAML_Auth_Simple('linkid-example');
+
+       if (!$as->isAuthenticated()) {
+
+            // initiate linkID login
+            $as->login(array(
+                'saml:idp' => 'linkID',
+                'ReturnTo' => 'http://localhost/linkid-example/index.php'
+                // This parameter is optional for running the linkID authentication process in a framed/popup mode.
+                // Please refer to the linkID SDK Manual for more information on this.
+                'DestinationParams' => '?login_mode=FRAMED'
+            ));
+
+    } else {
+
+header("Location: ./index.php");
+
+    }
+?>
