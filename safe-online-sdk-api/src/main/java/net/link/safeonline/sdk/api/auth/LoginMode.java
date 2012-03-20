9ee2e58ecc50f52910044e98286b3803d906a151
Stein Desmet <stein.desmet@link-k.net>
2012-03-20 13:51:58 +0100

    add parse methods

--- safe-online-sdk-api/src/main/java/net/link/safeonline/sdk/api/auth/LoginMode.java
+++ safe-online-sdk-api/src/main/java/net/link/safeonline/sdk/api/auth/LoginMode.java
@@ -1,5 +1,8 @@
 package net.link.safeonline.sdk.api.auth;
 
+import java.util.EnumSet;
+
+
 /**
 * Created by IntelliJ IDEA.
 * User: sgdesmet
@@ -9,4 +12,20 @@ package net.link.safeonline.sdk.api.auth;
 */
 public enum LoginMode {
     REDIRECT, POPUP, FRAMED, FRAMED_NO_BREAKFRAME, POPUP_NO_CLOSE;
+
+    public static LoginMode fromString(String text){
+        for (LoginMode type : EnumSet.allOf( LoginMode.class )){
+            if (type.toString().equalsIgnoreCase( text ))
+                return type;
+        }
+        return null;
+    }
+
+    public static LoginMode fromString(String text, LoginMode fallback){
+        for (LoginMode type : EnumSet.allOf( LoginMode.class )){
+            if (type.toString().equalsIgnoreCase( text ))
+                return type;
+        }
+        return fallback;
+    }
 }
