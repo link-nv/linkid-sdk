ca31cf1620643b796d81fe3557753719937c85eb
Stein Desmet <stein.desmet@link-k.net>
2012-05-16 11:07:56 +0200

    add an enum

---
+++ safe-online-sdk-api/src/main/java/net/link/safeonline/sdk/api/auth/ForceAuth.java
@@ -0,0 +1,13 @@
+package net.link.safeonline.sdk.api.auth;
+
+/**
+ * TODO description
+ * <p/>
+ * Date: 16/05/12
+ * Time: 11:06
+ *
+ * @author: sgdesmet
+ */
+public enum ForceAuth {
+    FORCE, AUTO
+}
