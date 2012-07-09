59d4064d24aa0d24cd12acfebb444ede4d846459
Wim Vandenhaute <wim.vandenhaute@gmail.com>
2012-07-09 12:12:36 +0200

    Add XKMS in PKI validation
    Adapt our XKMS2 WS Client to support UseKeyWith

--- safe-online-sdk/src/main/java/net/link/safeonline/sdk/ws/xkms2/Xkms2ClientImpl.java
+++ safe-online-sdk/src/main/java/net/link/safeonline/sdk/ws/xkms2/Xkms2ClientImpl.java
@@ -19,6 +19,7 @@ import net.link.safeonline.sdk.api.ws.xkms2.client.Xkms2Client;
 import net.link.safeonline.ws.xkms2.Xkms2ServiceFactory;
 import net.link.util.common.CertificateChain;
 import net.link.util.ws.AbstractWSClient;
+import org.jetbrains.annotations.Nullable;
 import org.w3._2000._09.xmldsig.KeyInfoType;
 import org.w3._2000._09.xmldsig.X509DataType;
 import org.w3._2002._03.xkms.*;
@@ -41,17 +42,35 @@ public class Xkms2ClientImpl extends AbstractWSClient<XKMSPortType> implements X
      */
     public Xkms2ClientImpl(String location, X509Certificate sslCertificate) {
 
+        this( location, SDKUtils.getSDKProperty( "linkid.ws.xkms2.path" ), sslCertificate );
+    }
+
+    /**
+     * Main constructor.
+     *
+     * @param location       the location (host:port) of the XKMS 2 web service.
+     * @param path           the path where the XKMS2 WS runs
+     * @param sslCertificate If not {@code null} will verify the server SSL {@link X509Certificate}.
+     */
+    public Xkms2ClientImpl(String location, String path, X509Certificate sslCertificate) {
+
         super( Xkms2ServiceFactory.newInstance().getXKMSPort(), sslCertificate );
-        getBindingProvider().getRequestContext()
-                .put( BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
-                        String.format( "%s/%s", location, SDKUtils.getSDKProperty( "linkid.ws.xkms2.path" ) ) );
+        getBindingProvider().getRequestContext().put( BindingProvider.ENDPOINT_ADDRESS_PROPERTY, String.format( "%s/%s", location, path ) );
     }
 
     @Override
     public void validate(final CertificateChain certificateChain)
             throws WSClientTransportException, ValidationFailedException, CertificateEncodingException {
 
-        logger.dbg( "validate" );
+        validate( null, null, certificateChain );
+    }
+
+    @Override
+    public void validate(@Nullable final String useKeyWithApplication, @Nullable final String useKeyWithIdentifier,
+                         final CertificateChain certificateChain)
+            throws WSClientTransportException, ValidationFailedException, CertificateEncodingException {
+
+        logger.dbg( "validate (useKeyWith: %s=%s)", useKeyWithApplication, useKeyWithIdentifier );
 
         ObjectFactory objectFactory = new ObjectFactory();
         org.w3._2000._09.xmldsig.ObjectFactory xmldsigObjectFactory = new org.w3._2000._09.xmldsig.ObjectFactory();
@@ -67,6 +86,14 @@ public class Xkms2ClientImpl extends AbstractWSClient<XKMSPortType> implements X
                     .add( xmldsigObjectFactory.createX509DataTypeX509Certificate( encodedCertificate ) );
         }
         keyInfo.getContent().add( xmldsigObjectFactory.createX509Data( x509Data ) );
+
+        if (null != useKeyWithApplication && null != useKeyWithIdentifier) {
+            UseKeyWithType useKeyWith = objectFactory.createUseKeyWithType();
+            useKeyWith.setApplication( useKeyWithApplication );
+            useKeyWith.setIdentifier( useKeyWithIdentifier );
+            queryKeyBinding.getUseKeyWith().add( useKeyWith );
+        }
+
         request.setQueryKeyBinding( queryKeyBinding );
 
         ValidateResultType result;
