45f03e8b401f15b1d638c14db9bc9b934be9bfdc
Wim Vandenhaute <wim.vandenhaute@gmail.com>
2012-08-09 11:23:01 +0200

    Fix: syntax error in php sdk example

--- safe-online-wicket-sdk/src/main/java/net/link/safeonline/wicket/component/linkid/LinkIDApplicationPage.java
+++ safe-online-wicket-sdk/src/main/java/net/link/safeonline/wicket/component/linkid/LinkIDApplicationPage.java
@@ -6,34 +6,31 @@
  */
 package net.link.safeonline.wicket.component.linkid;
 
+import com.lyndir.lhunath.opal.system.logging.Logger;
 import net.link.safeonline.wicket.LinkIDSession;
 import net.link.safeonline.wicket.annotation.linkid.RequireLogin;
 import net.link.util.wicket.component.WicketPage;
 import net.link.util.wicket.util.RedirectToPageException;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.apache.wicket.Page;
-import org.apache.wicket.PageParameters;
-import org.apache.wicket.Session;
+import org.apache.wicket.*;
 import org.apache.wicket.markup.html.WebPage;
 
 
 /**
  * <h2>{@link LinkIDApplicationPage}<br>
  * <sub>An abstract {@link WebPage} that provides some linkID-specific features to web applications.</sub></h2>
- *
+ * <p/>
  * <p>
  * This page takes care of synchronizing the application-level user with the linkID user. This synchronization involves asking the
  * application to create an application-level user after a successful linkID login has been completed, and invalidating the wicket
  * {@link Session} when the linkID user has changed or been logged out.
  * </p>
- *
+ * <p/>
  * <p>
  * We also provide a post-authentication mechanism which is used when users go to a page that requires authentication without being
  * authenticated yet. The user is sent to an authentication page (as defined by {@link RequireLogin#loginPage()}), and after a successful
  * authentication the user is sent back to the page he first tried to view. (See {@link #postAuth()})
  * </p>
- *
+ * <p/>
  * <p>
  * <i>Dec 15, 2008</i>
  * </p>
@@ -42,28 +39,27 @@ import org.apache.wicket.markup.html.WebPage;
  */
 public abstract class LinkIDApplicationPage extends WicketPage {
 
-    private static final Log LOG = LogFactory.getLog( LinkIDApplicationPage.class );
+    private static final Logger logger = Logger.get( LinkIDApplicationPage.class );
 
-    public LinkIDApplicationPage() {
+    protected LinkIDApplicationPage() {
 
-        super();
     }
 
-    public LinkIDApplicationPage(PageParameters parameters) {
+    protected LinkIDApplicationPage(PageParameters parameters) {
 
         super( parameters );
     }
 
     /**
      * If application created user successfully, perform post-authentication.
-     *
+     * <p/>
      * <p>
      * Post Authentication will redirect the user back to the page he was on when he was forced to authenticate before being allowed to see
      * the page. If there is no post authentication page set, nothing is done here.
      * </p>
-     *
+     * <p/>
      * <p>
-     * Called by {@link LinkIDApplicationPage} after successfully logging in with linkID.<br>
+     * Called after successfully logging in with linkID.<br>
      * You should call this method manually after application-specific authentication has completed, to support the post authentication
      * mechanism.
      * </p>
@@ -72,10 +68,11 @@ public abstract class LinkIDApplicationPage extends WicketPage {
 
         if (LinkIDSession.get().isUserSet()) {
             Class<? extends Page> postAuthPage = LinkIDSession.get().getPostAuthenticationPage();
-            PageParameters postAuthPageParameters = (LinkIDSession.get().getPostAuhtenticationParameters() != null?new PageParameters( LinkIDSession.get().getPostAuhtenticationParameters() ):null);
+            PageParameters postAuthPageParameters = LinkIDSession.get().getPostAuhtenticationParameters() != null? new PageParameters(
+                    LinkIDSession.get().getPostAuhtenticationParameters() ): null;
 
             if (postAuthPage != null) {
-                LOG.debug( "[LinkIDWicketAuth] auth completed; triggering post auth, sending user to " + postAuthPage );
+                logger.dbg( "[LinkIDWicketAuth] auth completed; triggering post auth, sending user to %s", postAuthPage );
 
                 LinkIDSession.get().setPostAuthenticationPage( null );
                 LinkIDSession.get().setPostAuhtenticationParameters( null );
@@ -90,7 +87,7 @@ public abstract class LinkIDApplicationPage extends WicketPage {
 
     /**
      * This method is invoked right after the linkID authentication succeeded.
-     *
+     * <p/>
      * <p>
      * More specifically, when a page is loaded but no application user is set and the linkID user is known, this method is called before
      * the constructor of your page.
