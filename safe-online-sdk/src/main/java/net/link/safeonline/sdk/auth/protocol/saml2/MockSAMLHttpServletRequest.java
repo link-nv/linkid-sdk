183507e9e7661b51656edb342acae0b7c16b7f26
Wim Vandenhaute <wim.vandenhaute@gmail.com>
2012-11-14 20:05:24 +0100

    Update for util-xxx changes

--- safe-online-sdk/src/main/java/net/link/safeonline/sdk/auth/protocol/saml2/MockSAMLHttpServletRequest.java
+++ safe-online-sdk/src/main/java/net/link/safeonline/sdk/auth/protocol/saml2/MockSAMLHttpServletRequest.java
@@ -13,16 +13,16 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequestWrapper;
-import org.apache.catalina.core.DummyRequest;
+import net.link.util.common.DummyServletRequest;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 
 /**
  * <h2>{@link MockSAMLHttpServletRequest}</h2>
- *
+ * <p/>
  * <p> [description / usage]. </p>
- *
+ * <p/>
  * <p> <i>Apr 15, 2009</i> </p>
  *
  * @author lhunath
@@ -35,9 +35,19 @@ public class MockSAMLHttpServletRequest extends HttpServletRequestWrapper {
 
     private final StringBuffer requestURL;
 
-    public MockSAMLHttpServletRequest(String queryString, StringBuffer requestURL) {
+    public MockSAMLHttpServletRequest(final String queryString, StringBuffer requestURL) {
 
-        super( new DummyRequest( "", requestURL.toString(), queryString ) );
+        //        super( new DummyServletRequest("", requestURL.toString(), queryString )));
+
+        //        super( new DummyRequest( "", requestURL.toString(), queryString ) );
+        super( new DummyServletRequest() {
+
+            @Override
+            public String getQueryString() {
+
+                return queryString;
+            }
+        } );
 
         parameters = new HashMap<String, String>();
         this.requestURL = requestURL;
@@ -53,59 +63,42 @@ public class MockSAMLHttpServletRequest extends HttpServletRequestWrapper {
 
                 parameters.put( URLDecoder.decode( kv[0], "UTF-8" ), URLDecoder.decode( kv[1], "UTF-8" ) );
             }
-        } catch (UnsupportedEncodingException e) {
+        }
+        catch (UnsupportedEncodingException e) {
             throw new RuntimeException( "UTF-8 encoding not supported.", e );
         }
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public StringBuffer getRequestURL() {
 
         return requestURL;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public String getParameter(String name) {
 
         return parameters.get( name );
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public Map<String, String> getParameterMap() {
 
         return parameters;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public Enumeration<String> getParameterNames() {
 
         return Collections.enumeration( parameters.keySet() );
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public String[] getParameterValues(String name) {
 
         return new String[] { getParameter( name ) };
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public String getMethod() {
 
