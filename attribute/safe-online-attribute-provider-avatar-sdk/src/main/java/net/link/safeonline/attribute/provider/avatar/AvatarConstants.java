145b3f6406b4121a3b1d2ae21d06dfaf01338b79
Stein Desmet <stein.desmet@link-k.net>
2013-01-02 12:00:53 +0100

    initial commit for avatar attribute provider

---
+++ attribute/safe-online-attribute-provider-avatar-sdk/src/main/java/net/link/safeonline/attribute/provider/avatar/AvatarConstants.java
@@ -0,0 +1,21 @@
+package net.link.safeonline.attribute.provider.avatar;
+
+/**
+ * TODO description
+ * <p/>
+ * Date: 28/12/12
+ * Time: 16:04
+ *
+ * @author: sgdesmet
+ */
+public interface AvatarConstants {
+
+    static final String AVATAR_IMAGE_COMPOUND_NAME = "avatar.image";
+    static final String AVATAR_IMAGE_URL_NAME      = "avatar.image.url";
+    static final String AVATAR_IMAGE_DATA_NAME     = "avatar.image.data";
+
+    static final String GROUP = "avatar";
+
+    static final int AVATAR_SIZE = 128;
+
+}
