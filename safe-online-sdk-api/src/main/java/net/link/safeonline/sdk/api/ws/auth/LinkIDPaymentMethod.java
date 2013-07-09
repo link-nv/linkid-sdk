3fcf70a8b0a96de76a161c777c44d010384478b0
Wim Vandenhaute <wim.vandenhaute@gmail.com>
2013-07-09 10:00:17 +0200

    Pass along attributeId to client
    (needed for removing tokens)

--- safe-online-sdk-api/src/main/java/net/link/safeonline/sdk/api/ws/auth/LinkIDPaymentMethod.java
+++ safe-online-sdk-api/src/main/java/net/link/safeonline/sdk/api/ws/auth/LinkIDPaymentMethod.java
@@ -6,17 +6,25 @@ import java.io.Serializable;
 @SuppressWarnings("UnusedDeclaration")
 public class LinkIDPaymentMethod implements Serializable {
 
+    private final String            attributeId;
     private final String            tokenId;
     private final LinkIDPaymentType tokenType;
     private final String            prettyPrint;
     private final int               priority;
 
-    public LinkIDPaymentMethod(final String tokenId, final LinkIDPaymentType tokenType, final String prettyPrint, final int priority) {
+    public LinkIDPaymentMethod(final String attributeId, final String tokenId, final LinkIDPaymentType tokenType, final String prettyPrint,
+                               final int priority) {
 
+        this.attributeId = attributeId;
         this.tokenId = tokenId;
         this.tokenType = tokenType;
         this.prettyPrint = prettyPrint;
         this.priority = priority;
+    }
+
+    public String getAttributeId() {
+
+        return attributeId;
     }
 
     public String getTokenId() {
