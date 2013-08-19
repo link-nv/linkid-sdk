53b7c01bd43e6c2caee8e1c2f6d950450d603b3d
Wim Vandenhaute <wim.vandenhaute@gmail.com>
2013-08-19 11:44:00 +0200

    LinkIDDevice: usage data fields

--- safe-online-sdk-api/src/main/java/net/link/safeonline/sdk/api/ws/auth/LinkIDDevice.java
+++ safe-online-sdk-api/src/main/java/net/link/safeonline/sdk/api/ws/auth/LinkIDDevice.java
@@ -1,6 +1,7 @@
 package net.link.safeonline.sdk.api.ws.auth;
 
 import java.io.Serializable;
+import java.util.Date;
 
 
 public class LinkIDDevice implements Serializable {
@@ -11,13 +12,22 @@ public class LinkIDDevice implements Serializable {
     private final String  info;
     private       boolean disabled;
 
-    public LinkIDDevice(final String name, final String friendlyName, final String id, final String info, final boolean disabled) {
+    // usage date
+    private final Date activationDate;
+    private final long authentications;
+    private final Date lastAuthenticated;
+
+    public LinkIDDevice(final String name, final String friendlyName, final String id, final String info, final boolean disabled, final Date activationDate,
+                        final long authentications, final Date lastAuthenticated) {
 
         this.name = name;
         this.friendlyName = friendlyName;
         this.id = id;
         this.info = info;
         this.disabled = disabled;
+        this.activationDate = activationDate;
+        this.authentications = authentications;
+        this.lastAuthenticated = lastAuthenticated;
     }
 
     public String getName() {
@@ -48,5 +58,20 @@ public class LinkIDDevice implements Serializable {
     public void setDisabled(final boolean disabled) {
 
         this.disabled = disabled;
+    }
+
+    public Date getActivationDate() {
+
+        return activationDate;
+    }
+
+    public long getAuthentications() {
+
+        return authentications;
+    }
+
+    public Date getLastAuthenticated() {
+
+        return lastAuthenticated;
     }
 }
