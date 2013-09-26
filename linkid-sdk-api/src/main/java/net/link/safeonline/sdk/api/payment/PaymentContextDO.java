9dd6a40b119586bdf5464d52b138b710d3e78b54
Wim Vandenhaute <wim.vandenhaute@gmail.com>
2013-09-26 13:34:37 +0200

    Deferred payments

--- linkid-sdk-api/src/main/java/net/link/safeonline/sdk/api/payment/PaymentContextDO.java
+++ linkid-sdk-api/src/main/java/net/link/safeonline/sdk/api/payment/PaymentContextDO.java
@@ -16,6 +16,7 @@ public class PaymentContextDO implements Serializable {
     public static final String PROFILE_KEY         = "PaymentContext.profile";
     public static final String VALIDATION_TIME_KEY = "PaymentContext.validationTime";
     public static final String ADD_LINK_KEY        = "PaymentContext.addLinkKey";
+    public static final String DEFERRED_PAY_KEY    = "PaymentContext.deferredPay";
 
     private final double   amount;
     private final Currency currency;
@@ -30,15 +31,20 @@ public class PaymentContextDO implements Serializable {
     // whether or not to display a link to linkID's add payment method page if the linkID user does not have any payment methods added, default is true.
     private final boolean showAddPaymentMethodLink;
 
+    // whether or not deferred payments are allowed, if a user has no payment token attached to the linkID account
+    // linkID can allow for the user to make a deferred payment which he can complete later on from his browser.
+    private final boolean allowDeferredPay;
+
     /**
      * @param amount                   amount in cents
      * @param currency                 currency
      * @param description              optional description
      * @param paymentProfile           optional payment profile
      * @param showAddPaymentMethodLink optional show add payment method link
+     * @param allowDeferredPay         optional allow deferred payments flag
      */
     public PaymentContextDO(final double amount, final Currency currency, @Nullable final String description, @Nullable final String paymentProfile,
-                            final int paymentValidationTime, final boolean showAddPaymentMethodLink) {
+                            final int paymentValidationTime, final boolean showAddPaymentMethodLink, final boolean allowDeferredPay) {
 
         this.amount = amount;
         this.currency = currency;
@@ -46,6 +52,7 @@ public class PaymentContextDO implements Serializable {
         this.paymentProfile = paymentProfile;
         this.paymentValidationTime = paymentValidationTime;
         this.showAddPaymentMethodLink = showAddPaymentMethodLink;
+        this.allowDeferredPay = allowDeferredPay;
     }
 
     /**
@@ -56,7 +63,7 @@ public class PaymentContextDO implements Serializable {
      */
     public PaymentContextDO(final double amount, final Currency currency, @Nullable final String description, @Nullable final String paymentProfile) {
 
-        this( amount, currency, description, paymentProfile, 5, true );
+        this( amount, currency, description, paymentProfile, 5, true, false );
     }
 
     /**
@@ -65,7 +72,7 @@ public class PaymentContextDO implements Serializable {
      */
     public PaymentContextDO(final double amount, final Currency currency) {
 
-        this( amount, currency, null, null, 5, true );
+        this( amount, currency, null, null, 5, true, false );
     }
 
     // Helper methods
@@ -82,6 +89,7 @@ public class PaymentContextDO implements Serializable {
             map.put( PROFILE_KEY, paymentProfile );
         map.put( VALIDATION_TIME_KEY, Integer.toString( paymentValidationTime ) );
         map.put( ADD_LINK_KEY, Boolean.toString( showAddPaymentMethodLink ) );
+        map.put( DEFERRED_PAY_KEY, Boolean.toString( allowDeferredPay ) );
 
         return map;
     }
@@ -103,7 +111,8 @@ public class PaymentContextDO implements Serializable {
         // convert
         return new PaymentContextDO( Double.parseDouble( paymentContextMap.get( AMOUNT_KEY ) ), Currency.parse( paymentContextMap.get( CURRENCY_KEY ) ),
                 paymentContextMap.get( DESCRIPTION_KEY ), paymentContextMap.get( PROFILE_KEY ),
-                Integer.parseInt( paymentContextMap.get( VALIDATION_TIME_KEY ) ), Boolean.parseBoolean( paymentContextMap.get( ADD_LINK_KEY ) ) );
+                Integer.parseInt( paymentContextMap.get( VALIDATION_TIME_KEY ) ), Boolean.parseBoolean( paymentContextMap.get( ADD_LINK_KEY ) ),
+                Boolean.parseBoolean( paymentContextMap.get( DEFERRED_PAY_KEY ) ) );
     }
 
     // Accessors
@@ -136,5 +145,10 @@ public class PaymentContextDO implements Serializable {
     public boolean isShowAddPaymentMethodLink() {
 
         return showAddPaymentMethodLink;
+    }
+
+    public boolean isAllowDeferredPay() {
+
+        return allowDeferredPay;
     }
 }
