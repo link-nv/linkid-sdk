e15719b601ba9103b605824e2ce15197c63b5e98
Wim Vandenhaute <wim.vandenhaute@gmail.com>
2014-03-21 14:16:50 +0100

    Moved qr-sdk to linkid-sdk-api

---
+++ linkid-sdk-api/src/main/java/net/link/safeonline/sdk/api/DeviceConstants.java
@@ -0,0 +1,11 @@
+package net.link.safeonline.sdk.api;
+
+/**
+ * Created by wvdhaute
+ * Date: 21/03/14
+ * Time: 14:09
+ */
+public abstract class DeviceConstants {
+
+    public static final String NAME = "qr";
+}
