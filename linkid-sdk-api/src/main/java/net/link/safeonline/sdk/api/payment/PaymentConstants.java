bce24fd435cf4d9ce53d23d58d56212c4235779e
Wim Vandenhaute <wim.vandenhaute@gmail.com>
2014-04-10 14:59:54 +0200

    Forgot to commit :|

--- linkid-sdk-api/src/main/java/net/link/safeonline/sdk/api/payment/PaymentConstants.java
+++ linkid-sdk-api/src/main/java/net/link/safeonline/sdk/api/payment/PaymentConstants.java
@@ -9,8 +9,6 @@ package net.link.safeonline.sdk.api.payment;
 
 public abstract class PaymentConstants {
 
-    @Deprecated
-    public static final String PAYMENT_CHANGED_ID_PARAM        = "txn_id";
     public static final String PAYMENT_CHANGED_ORDER_REF_PARAM = "orderRef";
 
     public static final String LTQR_ORDER_REF_PARAM         = "orderRef";
