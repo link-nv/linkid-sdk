47d50a396d849960325349c04e900f9f3e803eba
Wim Vandenhaute <wim.vandenhaute@gmail.com>
2014-04-14 14:13:02 +0200

    DocData attributes are deprecated

--- linkid-sdk-api/src/main/java/net/link/safeonline/sdk/api/payment/docdata/DocDataTokenType.java
+++ linkid-sdk-api/src/main/java/net/link/safeonline/sdk/api/payment/docdata/DocDataTokenType.java
@@ -5,7 +5,7 @@
  * Lin.k N.V. proprietary/confidential. Use is subject to license terms.
  */
 
-package net.link.safeonline.attribute.provider.docdata;
+package net.link.safeonline.sdk.api.payment.docdata;
 
 public enum DocDataTokenType {
 
