25680c2f6d05851872d421a47ebfb43177116328
Wim Vandenhaute <wim.vandenhaute@gmail.com>
2014-07-23 15:52:00 +0200

    example-rest moved to seperate repo

--- examples/example-rest/src/main/java/net/link/safeonline/sdk/example/rest/CacheControlDefaults.java
+++
@@ -1,9 +0,0 @@
-package net.link.safeonline.sdk.example.rest;
-
-import javax.ws.rs.core.CacheControl;
-
-
-public class CacheControlDefaults {
-
-    public static final CacheControl NO_STORE = CacheControl.valueOf( "no-store" );
-}
