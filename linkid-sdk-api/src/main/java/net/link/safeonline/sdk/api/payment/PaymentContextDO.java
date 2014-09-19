f09adc92b1a2f178e64bc894f7f7b54c407cd33e
Wim Vandenhaute <wim.vandenhaute@gmail.com>
2014-09-19 16:51:17 +0200

    PaymentAddBrowser option

--- linkid-sdk-api/src/main/java/net/link/safeonline/sdk/api/payment/PaymentContextDO.java
+++ linkid-sdk-api/src/main/java/net/link/safeonline/sdk/api/payment/PaymentContextDO.java
@@ -23,8 +23,7 @@ public class PaymentContextDO implements Serializable {
     public static final String ORDER_REFERENCE_KEY     = "PaymentContext.orderReference";
     public static final String PROFILE_KEY             = "PaymentContext.profile";
     public static final String VALIDATION_TIME_KEY     = "PaymentContext.validationTime";
-    public static final String ADD_LINK_KEY            = "PaymentContext.addLinkKey";
-    public static final String RETURN_MENU_URL_KEY     = "PaymentContext.returnMenuURL";
+    public static final String ADD_BROWSER_KEY         = "PaymentContext.addBrowser";
     public static final String DEFERRED_PAY_KEY        = "PaymentContext.deferredPay";
     public static final String MANDATE_KEY             = "PaymentContext.mandate";
     public static final String MANDATE_DESCRIPTION_KEY = "PaymentContext.mandateDescription";
@@ -48,15 +47,12 @@ public class PaymentContextDO implements Serializable {
     // maximum time to wait for payment validation, if not specified defaults to 5s
     private final int paymentValidationTime;
 
-    // whether or not to display a link to linkID's add payment method page if the linkID user does not have any payment methods added, default is true.
-    private final boolean showAddPaymentMethodLink;
+    // whether or not to allow to display the option in the client to add a payment method in the browser. Either via a popup or via a redirect made by the SP
+    // default is not allowed
+    private final PaymentAddBrowser paymentAddBrowser;
 
-    // if so and linkID user selects add payment method, the payment menu URL to redirect to will be returned in the payment response
-    // this is an alternative to "showAddPaymentMethodLink" where the payment menu is loaded via the iframe/popup. popup blockers...
-    private final boolean returnPaymentMenuURL;
-
-    // whether or not deferred payments are allowed, if a user has no payment token attached to the linkID account
-    // linkID can allow for the user to make a deferred payment which he can complete later on from his browser.
+    // whether or not deferred payments are allowed. An e-mail will be sent to the user to complete the payment at a later time.
+    // default is not allowed
     private final boolean allowDeferredPay;
 
     // mandates
@@ -64,24 +60,22 @@ public class PaymentContextDO implements Serializable {
     private final String  mandateDescription;
     private final String  mandateReference;
 
-    // optional payment menu return URLs (returnPaymentMenuURL)
+    // optional payment menu return URLs (docdata payment menu)
     private String paymentMenuResultSuccess;
     private String paymentMenuResultCanceled;
     private String paymentMenuResultPending;
     private String paymentMenuResultError;
 
     /**
-     * @param amount                   amount in cents
-     * @param currency                 currency
-     * @param description              optional description
-     * @param paymentProfile           optional payment profile
-     * @param showAddPaymentMethodLink optional show add payment method link
-     * @param allowDeferredPay         optional allow deferred payments flag
+     * @param amount         amount in cents
+     * @param currency       currency
+     * @param description    optional description
+     * @param paymentProfile optional payment profile
      */
     public PaymentContextDO(final double amount, final Currency currency, @Nullable final String description, @Nullable final String orderReference,
-                            @Nullable final String paymentProfile, final int paymentValidationTime, final boolean showAddPaymentMethodLink,
-                            final boolean returnPaymentMenuURL, final boolean allowDeferredPay, final boolean mandate,
-                            @Nullable final String mandateDescription, @Nullable final String mandateReference) {
+                            @Nullable final String paymentProfile, final int paymentValidationTime, final PaymentAddBrowser paymentAddBrowser,
+                            final boolean allowDeferredPay, final boolean mandate, @Nullable final String mandateDescription,
+                            @Nullable final String mandateReference) {
 
         this.amount = amount;
         this.currency = currency;
@@ -89,8 +83,7 @@ public class PaymentContextDO implements Serializable {
         this.orderReference = orderReference;
         this.paymentProfile = paymentProfile;
         this.paymentValidationTime = paymentValidationTime;
-        this.showAddPaymentMethodLink = showAddPaymentMethodLink;
-        this.returnPaymentMenuURL = returnPaymentMenuURL;
+        this.paymentAddBrowser = paymentAddBrowser;
         this.allowDeferredPay = allowDeferredPay;
 
         this.mandate = mandate;
@@ -99,19 +92,16 @@ public class PaymentContextDO implements Serializable {
     }
 
     /**
-     * @param amount                   amount in cents
-     * @param currency                 currency
-     * @param description              optional description
-     * @param paymentProfile           optional payment profile
-     * @param showAddPaymentMethodLink optional show add payment method link
-     * @param allowDeferredPay         optional allow deferred payments flag
+     * @param amount         amount in cents
+     * @param currency       currency
+     * @param description    optional description
+     * @param paymentProfile optional payment profile
      */
     public PaymentContextDO(final double amount, final Currency currency, @Nullable final String description, @Nullable final String orderReference,
-                            @Nullable final String paymentProfile, final int paymentValidationTime, final boolean showAddPaymentMethodLink,
+                            @Nullable final String paymentProfile, final int paymentValidationTime, final PaymentAddBrowser paymentAddBrowser,
                             final boolean allowDeferredPay) {
 
-        this( amount, currency, description, orderReference, paymentProfile, paymentValidationTime, showAddPaymentMethodLink, false, allowDeferredPay, false,
-                null, null );
+        this( amount, currency, description, orderReference, paymentProfile, paymentValidationTime, paymentAddBrowser, allowDeferredPay, false, null, null );
     }
 
     /**
@@ -123,7 +113,7 @@ public class PaymentContextDO implements Serializable {
     public PaymentContextDO(final double amount, final Currency currency, @Nullable final String description, @Nullable final String orderReference,
                             @Nullable final String paymentProfile) {
 
-        this( amount, currency, description, orderReference, paymentProfile, 5, true, false );
+        this( amount, currency, description, orderReference, paymentProfile, 5, PaymentAddBrowser.NOT_ALLOWED, false );
     }
 
     /**
@@ -132,7 +122,7 @@ public class PaymentContextDO implements Serializable {
      */
     public PaymentContextDO(final double amount, final Currency currency) {
 
-        this( amount, currency, null, null, null, 5, true, false );
+        this( amount, currency, null, null, null, 5, PaymentAddBrowser.NOT_ALLOWED, false );
     }
 
     // Helper methods
@@ -150,8 +140,7 @@ public class PaymentContextDO implements Serializable {
         if (null != paymentProfile)
             map.put( PROFILE_KEY, paymentProfile );
         map.put( VALIDATION_TIME_KEY, Integer.toString( paymentValidationTime ) );
-        map.put( ADD_LINK_KEY, Boolean.toString( showAddPaymentMethodLink ) );
-        map.put( RETURN_MENU_URL_KEY, Boolean.toString( returnPaymentMenuURL ) );
+        map.put( ADD_BROWSER_KEY, paymentAddBrowser.name() );
         map.put( DEFERRED_PAY_KEY, Boolean.toString( allowDeferredPay ) );
 
         map.put( MANDATE_KEY, Boolean.toString( mandate ) );
@@ -190,9 +179,9 @@ public class PaymentContextDO implements Serializable {
         PaymentContextDO paymentContextDO = new PaymentContextDO( Double.parseDouble( paymentContextMap.get( AMOUNT_KEY ) ),
                 Currency.parse( paymentContextMap.get( CURRENCY_KEY ) ), paymentContextMap.get( DESCRIPTION_KEY ), paymentContextMap.get( ORDER_REFERENCE_KEY ),
                 paymentContextMap.get( PROFILE_KEY ), Integer.parseInt( paymentContextMap.get( VALIDATION_TIME_KEY ) ),
-                Boolean.parseBoolean( paymentContextMap.get( ADD_LINK_KEY ) ), Boolean.parseBoolean( paymentContextMap.get( RETURN_MENU_URL_KEY ) ),
-                Boolean.parseBoolean( paymentContextMap.get( DEFERRED_PAY_KEY ) ), Boolean.parseBoolean( paymentContextMap.get( MANDATE_KEY ) ),
-                paymentContextMap.get( MANDATE_DESCRIPTION_KEY ), paymentContextMap.get( MANDATE_REFERENCE_KEY ) );
+                PaymentAddBrowser.parse( paymentContextMap.get( ADD_BROWSER_KEY ) ), Boolean.parseBoolean( paymentContextMap.get( DEFERRED_PAY_KEY ) ),
+                Boolean.parseBoolean( paymentContextMap.get( MANDATE_KEY ) ), paymentContextMap.get( MANDATE_DESCRIPTION_KEY ),
+                paymentContextMap.get( MANDATE_REFERENCE_KEY ) );
 
         paymentContextDO.setPaymentMenuResultSuccess( paymentContextMap.get( MENU_RESULT_SUCCESS_KEY ) );
         paymentContextDO.setPaymentMenuResultCanceled( paymentContextMap.get( MENU_RESULT_CANCELED_KEY ) );
@@ -206,11 +195,10 @@ public class PaymentContextDO implements Serializable {
     public String toString() {
 
         return String.format( "Amount: %f, Currency: %s, Description: \"%s\", OrderReference: \"%s\", Profile: \"%s\", validationTime: %d, "
-                              + "addPaymentMethodLink: %s, returnPaymentMenuURL: %s allowDeferredPay: %s, "
-                              + "mandate: %s, mandateDescription: %s, mandateReference: %s "
+                              + "paymentAddBrowser: %s, allowDeferredPay: %s, mandate: %s, mandateDescription: %s, mandateReference: %s "
                               + "menuResultSuccess: %s, menuResultCanceled: %s, menuResultPending: %s, menuResultError: %s", amount, currency, description,
-                orderReference, paymentProfile, paymentValidationTime, showAddPaymentMethodLink, returnPaymentMenuURL, allowDeferredPay, mandate,
-                mandateDescription, mandateReference, paymentMenuResultSuccess, paymentMenuResultCanceled, paymentMenuResultPending, paymentMenuResultError );
+                orderReference, paymentProfile, paymentValidationTime, paymentAddBrowser, allowDeferredPay, mandate, mandateDescription, mandateReference,
+                paymentMenuResultSuccess, paymentMenuResultCanceled, paymentMenuResultPending, paymentMenuResultError );
     }
 
     // Accessors
@@ -245,14 +233,9 @@ public class PaymentContextDO implements Serializable {
         return paymentValidationTime;
     }
 
-    public boolean isShowAddPaymentMethodLink() {
+    public PaymentAddBrowser getPaymentAddBrowser() {
 
-        return showAddPaymentMethodLink;
-    }
-
-    public boolean isReturnPaymentMenuURL() {
-
-        return returnPaymentMenuURL;
+        return paymentAddBrowser;
     }
 
     public boolean isAllowDeferredPay() {
