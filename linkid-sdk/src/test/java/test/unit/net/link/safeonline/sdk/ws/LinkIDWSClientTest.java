ef140cf0773acfea178b6a3d43e74cbc591623a3
Wim Vandenhaute <wim.vandenhaute@gmail.com>
2015-06-30 16:33:19 +0200

    Cleanup

--- linkid-sdk/src/test/java/test/unit/net/link/safeonline/sdk/ws/LinkIDWSClientTest.java
+++ linkid-sdk/src/test/java/test/unit/net/link/safeonline/sdk/ws/LinkIDWSClientTest.java
@@ -428,7 +428,7 @@ public class LinkIDWSClientTest {
         client.capture( orderReference );
     }
 
-    @Test
+    //    @Test
     public void testGetLocalization()
             throws Exception {
 
